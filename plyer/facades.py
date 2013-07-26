16309c06306763fff8dbf49fdaa36e3b9553efa5
Ben Rousch <brousch@gmail.com>
2013-07-25 23:11:48 -0400

    Changed NotImplemented exception to NotImplementedError

--- plyer/facades.py
+++ plyer/facades.py
@@ -8,6 +8,7 @@ Interface of all the features availables.
 
 __all__ = ('Accelerometer', 'Camera', 'Notification', 'TTS')
 
+
 class Accelerometer(object):
     '''Accelerometer facade.
     '''
@@ -35,13 +36,13 @@ class Accelerometer(object):
     # private
 
     def _enable(self):
-        raise NotImplemented()
+        raise NotImplementedError()
 
     def _disable(self):
-        raise NotImplemented()
+        raise NotImplementedError()
 
     def _get_acceleration(self):
-        raise NotImplemented()
+        raise NotImplementedError()
 
 
 class Camera(object):
@@ -66,7 +67,7 @@ class Camera(object):
     # private
 
     def _take_picture(self, **kwargs):
-        raise NotImplemented()
+        raise NotImplementedError()
 
 
 class Notification(object):
@@ -86,7 +87,7 @@ class Notification(object):
     # private
 
     def _notify(self, **kwargs):
-        raise NotImplemented()
+        raise NotImplementedError()
 
 
 class TTS(object):
@@ -104,5 +105,5 @@ class TTS(object):
     # private
 
     def _speak(self, **kwargs):
-        raise NotImplemented()
+        raise NotImplementedError()
 
