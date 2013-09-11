5c3943919c57720a46197c600e34db7a14a3229e
Ben Rousch <brousch@gmail.com>
2013-09-11 09:37:13 -0400

    PEPed up balloontip. Fixed no 2nd notify bug. Fixed blocking notification bug.

--- plyer/platforms/win/notification.py
+++ plyer/platforms/win/notification.py
@@ -1,12 +1,14 @@
+import thread
+
 from plyer.facades import Notification
 from libs.balloontip import balloon_tip
 
 
 class WindowsNotification(Notification):
     def _notify(self, **kwargs):
-        balloon_tip(kwargs.get('title'), kwargs.get('message'))
+        thread.start_new_thread(balloon_tip,
+                                (kwargs.get('title'), kwargs.get('message')))
 
 
 def instance():
     return WindowsNotification()
-
