dfe2ef6a27b11a7ef30fdfe6bb9d82ee49bd3a99
Ben Rousch <brousch@gmail.com>
2013-09-13 10:22:21 -0400

    Fixed hasattr bug. Added default icon support for Android notifications

--- plyer/platforms/android/notification.py
+++ plyer/platforms/android/notification.py
@@ -5,24 +5,30 @@ from plyer.platforms.android import activity
 AndroidString = autoclass('java.lang.String')
 PythonActivity = autoclass('org.renpy.android.PythonActivity')
 NotificationBuilder = autoclass('android.app.Notification$Builder')
-#Drawable = autoclass('net.clusterbleep.notificationdemo.R$drawable')
+
+# Find the Drawables for this app
+application_info = PythonActivity.getApplicationInfo().toString()
+# Something like: 'ApplicationInfo{421aa0e8 net.clusterbleep.kivyremoteshell}'
+package_name = application_info.split(' ')[1][:-1]
+Drawable = autoclass(package_name+'.R$drawable')
+
 
 class AndroidNotification(Notification):
     def _get_notification_service(self):
-        if not hasattr('_ns'):
+        if not hasattr(self, '_ns'):
             self._ns = activity.getSystemService(
                     PythonActivity.NOTIFICATION_SERVICE)
         return self._ns
 
     def _notify(self, **kwargs):
-        #icon = Drawable.icon
+        icon = Drawable.icon
         noti = NotificationBuilder(activity)
         #noti.setDefaults(Notification.DEFAULT_ALL)
         noti.setContentTitle(AndroidString(
             kwargs.get('title').encode('utf-8')))
         noti.setContentText(AndroidString(
             kwargs.get('message').encode('utf-8')))
-        #noti.setSmallIcon(icon)
+        noti.setSmallIcon(icon)
         noti.setAutoCancel(True)
         self._get_notification_service().notify(0, noti.build())
 
