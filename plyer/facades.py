d31e02c0a994fcf6d6a09a146d3554dec30dd19c
Alexander Taylor <alexanderjohntaylor@gmail.com>
2014-01-27 22:00:55 +0000

    Completed email facade and android implementation

--- plyer/facades.py
+++ plyer/facades.py
@@ -94,12 +94,20 @@ class Email(object):
     '''Email facade.'''
 
     def send(self, recipient=None, subject=None, text=None,
-             attachment=None):
+             create_chooser=None):
         '''Open an email client message send window, prepopulated with the
-        given arguments.'''
-        print 'EMAIL FACADE USED'
+        given arguments.
+
+        :param recipient: Recipient of the message (str)
+        :param subject: Subject of the message (str)
+        :param text: Main body of the message (str)
+        :param create_chooser: Whether to display a program chooser to
+                               handle the message (bool)
+
+        .. note:: create_chooser is only supported on Android
+        '''
         self._send(recipient=recipient, subject=subject, text=text,
-                   attachment=None)
+                   create_chooser=create_chooser)
 
     # private
 
