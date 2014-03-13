10e2d728538d4d87aba49eef90ff13acdf5c20fd
gtrivedi <gtrivedi@nitk.ac.in>
2014-03-13 09:45:41 -0400

    Added a simple accelerometer example

---
+++ examples/accelerometer/basic/main.py
@@ -0,0 +1,49 @@
+'''
+Basic accelerometer example.
+'''
+
+from kivy.lang import Builder
+from kivy.app import App
+from kivy.properties import ObjectProperty
+from kivy.uix.boxlayout import BoxLayout
+from kivy.clock import Clock
+
+from plyer import accelerometer
+
+class AccelerometerTest(BoxLayout):
+    def __init__(self):
+        super(AccelerometerTest, self).__init__()
+        self.accel_status = ""
+        self.sensorEnabled = False
+
+    def do_toggle(self):
+        try:
+            if not self.sensorEnabled:
+                accelerometer.enable()
+                Clock.schedule_interval(self.get_acceleration, 1 / 20.)
+
+                self.sensorEnabled = True
+                self.ids.toggle_button.text = "Stop Accelerometer"
+            else:
+                accelerometer.disable()
+                Clock.unschedule(self.get_acceleration)
+
+                self.sensorEnabled = False
+                self.ids.toggle_button.text = "Start Accelerometer"
+        except NotImplementedError:
+            import traceback; traceback.print_exc()
+            self.ids.accel_status.text = "Accelerometer is not implemented for your platform"
+
+    def get_acceleration(self, dt):
+        val = accelerometer.acceleration
+
+        self.ids.x_label.text = "X: " + str(val[0])
+        self.ids.y_label.text = "Y: " + str(val[1])
+        self.ids.z_label.text = "Z: " + str(val[2])
+
+class AccelerometerTestApp(App):
+    def build(self):
+        return AccelerometerTest()
+
+if __name__ == '__main__':
+    AccelerometerTestApp().run()
