57c0be4f76adfde05cc684bff0feb43a34a600b9
Davide Depau <me@davideddu.org>
2014-04-01 21:51:18 -0400

    add Mac OS X email support

---
+++ plyer/platforms/macosx/email.py
@@ -0,0 +1,28 @@
+import subprocess
+from urllib import quote
+from plyer.facades import Email
+
+class MacOSXEmail(Email):
+    def _send(self, **kwargs):
+        recipient = kwargs.get('recipient')
+        subject = kwargs.get('subject')
+        text = kwargs.get('text')
+        create_chooser = kwargs.get('create_chooser')
+
+        uri = "mailto:"
+        if recipient:
+            uri += str(recipient)
+        if subject:
+            uri += "?" if not "?" in uri else "&"
+            uri += "subject="
+            uri += quote(str(subject))
+        if text:
+            uri += "?" if not "?" in uri else "&"
+            uri += "body="
+            uri += quote(str(text))
+
+        subprocess.Popen(["open", uri])
+
+
+def instance():
+    return MacOSXEmail()
