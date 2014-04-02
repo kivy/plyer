2191e253b35f35f3767c693e6e085abdf490c069
Davide Depau <me@davideddu.org>
2014-04-01 22:51:45 -0400

    fix flipped accelerations on Linux

--- plyer/platforms/linux/accelerometer.py
+++ plyer/platforms/linux/accelerometer.py
@@ -29,7 +29,8 @@ class LinuxAccelerometer(Accelerometer):
             coords = re.findall(r"[-]?\d+\.?\d*", t)
             # Apparently the acceleration on sysfs goes from -1000 to 1000.
             # I divide it by 100 to make it equivalent to Android.
-            return [float(i)/100 for i in coords]
+            # The negative is because the coordinates are inverted on Linux
+            return [float(i)/-100 for i in coords]
 
 def instance():
     return LinuxAccelerometer()
