0d68fbaef300c53db407f6296c00e493e4b040bf
Davide Depau <me@davideddu.org>
2014-04-03 16:35:50 -0400

    use xdg-email by default, fallback to xdg-open + mailto uri

--- plyer/platforms/linux/email.py
+++ plyer/platforms/linux/email.py
@@ -10,18 +10,28 @@ class LinuxEmail(Email):
         create_chooser = kwargs.get('create_chooser')
 
         uri = "mailto:"
+        args = ["xdg-email"]
         if recipient:
             uri += str(recipient)
+            args += [str(recipient)]
         if subject:
             uri += "?" if not "?" in uri else "&"
             uri += "subject="
             uri += quote(str(subject))
+            args += ["--subject", str(subject)]
         if text:
             uri += "?" if not "?" in uri else "&"
             uri += "body="
             uri += quote(str(text))
+            args += ["--body", str(text)]
 
-        subprocess.Popen(["xdg-open", uri])
+        try:
+            subprocess.Popen(args)
+        except OSError:
+            try:
+                subprocess.Popen(["xdg-open", uri])
+            except OSError:
+                print "Warning: unable to start an email client. Make sure xdg-open is installed."
 
 
 def instance():
