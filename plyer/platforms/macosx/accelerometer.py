012cffd958efe257e379f7d6b5f9296b2f2e944d
gtrivedi <trivedigaurav@gmail.com>
2014-05-31 17:20:27 -0400

    Switched to ctypes

--- plyer/platforms/macosx/accelerometer.py
+++ plyer/platforms/macosx/accelerometer.py
@@ -4,18 +4,21 @@ MacOSX accelerometer
 '''
 
 from plyer.facades import Accelerometer
-from plyer.platforms.macosx import motionsensor
+from plyer.platforms.macosx.libs import osx_motion_sensor
 
 class OSXAccelerometer(Accelerometer):
 
     def _enable(self):
-        return motionsensor.is_available()
+    	try:
+			osx_motion_sensor.get_coord()
+        except:
+        	raise Exception('Could not enable motion sensor on this macbook!')
 
     def _disable(self):
         pass
 
     def _get_acceleration(self):
-        return motionsensor.get_coord()
+        return osx_motion_sensor.get_coord()
         
 def instance():
     return OSXAccelerometer()
