fbd435edd4adb723620dead03ac3f342f679ff6b
gtrivedi <trivedigaurav@gmail.com>
2014-06-10 14:23:18 -0400

    Remove dependency on Hardware.java

--- plyer/platforms/android/gyroscope.py
+++ plyer/platforms/android/gyroscope.py
@@ -4,20 +4,58 @@ Android Gyroscope
 '''
 
 from plyer.facades import Gyroscope
-from jnius import autoclass
+from jnius import PythonJavaClass, java_method, autoclass, cast
+from plyer.platforms.android import activity
 
-Hardware = autoclass('org.renpy.android.Hardware')
+Context = autoclass('android.content.Context')
+Sensor = autoclass('android.hardware.Sensor')
+SensorManager = autoclass('android.hardware.SensorManager')
+
+class GyroscopeSensorListener(PythonJavaClass):
+    __javainterfaces__ = ['android/hardware/SensorEventListener']
+
+    def __init__(self):
+        super(GyroscopeSensorListener, self).__init__()
+        self.SensorManager = cast('android.hardware.SensorManager', 
+                    activity.getSystemService(Context.SENSOR_SERVICE))
+        self.sensor = self.SensorManager.getDefaultSensor(
+                Sensor.TYPE_GYROSCOPE)
+        
+        self.values = [0, 0, 0]
+
+    def enable(self):
+        self.SensorManager.registerListener(self, self.sensor,
+                    SensorManager.SENSOR_DELAY_NORMAL)
+
+    def disable(self):
+        self.SensorManager.unregisterListener(self, self.sensor)
+
+    @java_method('()I')
+    def hashCode(self):
+        return id(self)
+
+    @java_method('(Landroid/hardware/SensorEvent;)V')
+    def onSensorChanged(self, event):
+        self.values = event.values[:3]
+
+    @java_method('(Landroid/hardware/Sensor;I)V')
+    def onAccuracyChanged(self, sensor, accuracy):
+        # Maybe, do something in future? 
+        pass 
 
 class AndroidGyroscope(Gyroscope):
+    def __init__(self):
+        super(AndroidGyroscope, self).__init__()
+        self.listener = GyroscopeSensorListener()
 
     def _enable(self):
-        Hardware.orientationSensorEnable(True)
+        self.listener.enable()
 
     def _disable(self):
-        Hardware.orientationSensorEnable(False)
+        self.listener.disable()
 
     def _get_orientation(self):
-        return Hardware.orientationSensorReading()
+        return tuple(self.listener.values)
 
 def instance():
     return AndroidGyroscope()
