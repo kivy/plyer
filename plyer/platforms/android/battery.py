8266158eabca97e7a872cd6c7657b01d083382c3
gtrivedi <trivedigaurav@gmail.com>
2014-06-29 18:06:13 -0400

    Fix android implemenation typos

--- plyer/platforms/android/battery.py
+++ plyer/platforms/android/battery.py
@@ -4,24 +4,24 @@ from plyer.facades import Battery
 
 Intent = autoclass('android.content.Intent')
 BatteryManager = autoclass('android.os.BatteryManager')
+IntentFilter = autoclass('android.content.IntentFilter')
 
 class AndroidBattery(Battery):
     def _get_status(self):
         status = {"connected": None, "percentage": None}
 
-        ifilter = cast('android.content.IntentFilter', 
-            Intent.ACTION_BATTERY_CHANGED)
+        ifilter = IntentFilter(Intent.ACTION_BATTERY_CHANGED)
 
         batteryStatus = cast('android.content.Intent', 
-            activity.registerReceiver(null, ifilter))
+            activity.registerReceiver(None, ifilter))
 
-        status = batteryStatus.getIntExtra(BatteryManager.EXTRA_STATUS, -1)
-        isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING ||
-                     status == BatteryManager.BATTERY_STATUS_FULL;
+        query = batteryStatus.getIntExtra(BatteryManager.EXTRA_STATUS, -1)
+        isCharging = (query == BatteryManager.BATTERY_STATUS_CHARGING or \
+                     query == BatteryManager.BATTERY_STATUS_FULL)
 
         level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1)
         scale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
-        percentage = level / (float)scale
+        percentage = level / float(scale)
 
         status['connected'] = isCharging
         status['percentage'] = percentage
