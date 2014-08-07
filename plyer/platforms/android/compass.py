74eaa5900638a25efd20234e3454b19ca76381de
gtrivedi <trivedigaurav@gmail.com>
2014-08-07 00:08:13 -0400

    Fix sensor enable-disable

--- plyer/platforms/android/compass.py
+++ plyer/platforms/android/compass.py
@@ -48,16 +48,30 @@ class MagneticFieldSensorListener(PythonJavaClass):
 class AndroidCompass(Compass):
     def __init__(self):
         super(AndroidCompass, self).__init__()
-        self.listener = MagneticFieldSensorListener()
+        self.bState = False
 
     def _enable(self):
-        self.listener.enable()
+        if (not self.bState):
+            self.listener = MagneticFieldSensorListener()
+            self.listener.enable()
+            self.bState = True
 
     def _disable(self):
-        self.listener.disable()
+        if (self.bState):
+            self.bState = False
+            self.listener.disable()
+            del self.listener
 
     def _get_orientation(self):
-        return tuple(self.listener.values)
+        if (self.bState):
+            return tuple(self.listener.values)
+        else:
+            return (None, None, None)
+
+    def __del__(self):
+        if(self.bState):
+            self._disable()
+        super(self.__class__, self).__del__()
 
 
 def instance():
