4cac191b13b9651d3c7e90f8c5dab10d654bd213
gtrivedi <trivedigaurav@gmail.com>
2014-08-10 16:07:54 -0400

    iOS UUID facade

---
+++ plyer/platforms/ios/uniqueid.py
@@ -0,0 +1,17 @@
+from pyobjus import autoclass
+from pyobjus.dylib_manager import load_framework
+from plyer.facades import UniqueID
+
+load_framework('/System/Library/Frameworks/UIKit.framework')
+UIDevice = autoclass('UIDevice')
+
+
+class iOSUniqueID(UniqueID):
+
+    def _get_uid(self):
+        uuid = UIDevice.currentDevice().identifierForVendor.UUIDString()
+        return uuid.UTF8String()
+
+
+def instance():
+    return iOSUniqueID()
