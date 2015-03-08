e677e1de24612f54ad3d40396806a76ee9af9155
gtrivedi <trivedigaurav@gmail.com>
2015-03-08 19:25:06 -0400

    use environ to change LANG to 'C' while calling shell processes

--- plyer/platforms/linux/battery.py
+++ plyer/platforms/linux/battery.py
@@ -2,9 +2,14 @@ from subprocess import Popen, PIPE
 from plyer.facades import Battery
 from plyer.utils import whereis_exe
 
+from os import environ
+
 
 class LinuxBattery(Battery):
     def _get_state(self):
+        old_lang = environ.get('LANG')
+        environ['LANG'] = 'C'
+
         status = {"isCharging": None, "percentage": None}
 
         # We are supporting only one battery now
@@ -12,6 +17,8 @@ class LinuxBattery(Battery):
         upower_process = Popen(["upower", "-d", dev],
                 stdout=PIPE)
         output = upower_process.communicate()[0]
+
+        environ['LANG'] = old_lang
 
         if not output:
             return status
