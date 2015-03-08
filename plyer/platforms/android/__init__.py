69f971fc9010e877c15c648f3a71e2d6f0fe4cf5
Ryan Pessa <ryan@essential-elements.net>
2015-03-07 21:10:58 -0600

    add IrBlaster facade and Android implementation

--- plyer/platforms/android/__init__.py
+++ plyer/platforms/android/__init__.py
@@ -1,7 +1,8 @@
 from os import environ
 from jnius import autoclass
 
-SDK_INT = autoclass('android.os.Build$VERSION').SDK_INT
+ANDROID_VERSION = autoclass('android.os.Build$VERSION')
+SDK_INT = ANDROID_VERSION.SDK_INT
 
 if 'PYTHON_SERVICE_ARGUMENT' in environ:
     PythonService = autoclass('org.renpy.android.PythonService')
