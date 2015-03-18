a7518de6bbe27e9b62c5e9acc02aa6d68c20ba5c
Andrzej Grymkowski <grymek4@gmail.com>
2015-03-18 23:41:56 +0100

    added query method

--- plyer/platforms/android/contacts.py
+++ plyer/platforms/android/contacts.py
@@ -17,10 +17,42 @@ class AndroidContacts(Contacts):
     .. versionadded:: 1.2.4
     """
 
+    def query(self, url, columns=None, where=None,
+              where_args=None, order_by=None):
+        """Gives cursor over the result set
+
+        Android query behaves a little like SQL queries.
+        With params you can read query as:
+            select `columns`
+            from `url`
+            where `where` % `where_args`
+            order by `order_by`
+
+        :param url: address from retrieve results
+        :param columns: selected columns to return. If none then return all
+        :param where: args to filter results.
+        :param where_args: list of strings replaced with `?` in where
+        :param order_by: order of result
+        :return: cursor with the results
+        """
+
+        # need to create here method .convertToJavaArray
+        if columns:
+            j_columns = ArrayList()
+            [j_columns.add(column) for column in columns]
+            columns = j_columns.toArray()
+
+        if where_args:
+            j_args = ArrayList()
+            [j_args.add(param) for param in where_args]
+            where_args = j_args.toArray()
+
+        cr = activity.getContentResolver()
+        return cr.query(url, columns, where, where_args, order_by)
+
     def refresh(self):
         """Refresh local contact list."""
-        cr = activity.getContentResolver()
-        contact_cr = cr.query(JavaContacts.CONTENT_URI, None, None, None, None)
+        contact_cr = self.query(JavaContacts.CONTENT_URI)
 
         contacts = []
         while contact_cr.moveToNext():
@@ -39,11 +71,11 @@ class AndroidContacts(Contacts):
 
             phone_numbers = []
             if contact['has_phone_number'] > 0:
-                l = ArrayList()
-                l.add(contact['contact_id'])
-                query = "contact_id=?"
+                where = "contact_id=?"
+                where_args = [contact['contact_id']]
                 phone_uri = Phone.CONTENT_URI
-                phone_cr = cr.query(phone_uri, None, query, l.toArray(), None)
+                phone_cr = self.query(phone_uri, where=where,
+                                      where_args=where_args)
 
                 while phone_cr.moveToNext():
                     phone_number = phone_cr.getColumnIndex(Phone.NUMBER)
