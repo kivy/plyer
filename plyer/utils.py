9912b6373047c8df35f103cf1519c5ccd82284a8
akshayaurora <akshayaurora@gmail.com>
2015-07-28 14:10:58 +0530

    Introduce camera access for ios and a example.

--- plyer/utils.py
+++ plyer/utils.py
@@ -4,7 +4,7 @@ Utils
 
 '''
 
-__all__ = ('platform', )
+__all__ = ('platform', 'reify')
 
 from os import environ
 from os import path
@@ -133,3 +133,36 @@ def whereis_exe(program):
             not path.isdir(path.join(p, program)):
             return path.join(p, program)
     return None
+
+class reify(object):
+    '''
+    Put the result of a method which uses this (non-data) descriptor decorator
+    in the instance dict after the first call, effectively replacing the
+    decorator with an instance variable.
+
+    It acts like @property, except that the function is only ever called once;
+    after that, the value is cached as a regular attribute. This gives you lazy
+    attribute creation on objects that are meant to be immutable.
+
+    Taken from the `Pyramid project <https://pypi.python.org/pypi/pyramid/>`_.
+
+    To use this as a decorator::
+
+         @reify
+         def lazy(self):
+              ...
+              return hard_to_compute_int
+         first_time = self.lazy   # lazy is reify obj, reify.__get__() runs
+         second_time = self.lazy  # lazy is hard_to_compute_int
+    '''
+
+    def __init__(self, func):
+        self.func = func
+        self.__doc__ = func.__doc__
+
+    def __get__(self, inst, cls):
+        if inst is None:
+            return self
+        retval = self.func(inst)
+        setattr(inst, self.func.__name__, retval)
+        return retval
