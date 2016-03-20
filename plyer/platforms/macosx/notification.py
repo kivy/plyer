802fd723f67bf16b26b07fc708453654606a8972
andong777 <andong777@163.com>
2016-03-20 13:20:16 +0800

    rewrite notification on mac using pyobjus

--- plyer/platforms/macosx/notification.py
+++ plyer/platforms/macosx/notification.py
@@ -1,27 +1,34 @@
 from plyer.facades import Notification
-import Foundation
-import objc
-import AppKit
+from pyobjus import autoclass, protocol, objc_str, ObjcBOOL
+from pyobjus.dylib_manager import load_framework, INCLUDE
+
+load_framework(INCLUDE.AppKit)
+load_framework(INCLUDE.Foundation)
+
+NSUserNotification = autoclass('NSUserNotification')
+NSUserNotificationCenter = autoclass('NSUserNotificationCenter')
 
 
 class OSXNotification(Notification):
     def _notify(self, **kwargs):
-        NSUserNotification = objc.lookUpClass('NSUserNotification')
-        NSUserNotificationCenter = objc.lookUpClass('NSUserNotificationCenter')
+        title = kwargs.get('title', '')
+        message = kwargs.get('message', '')
+        app_name = kwargs.get('app_name', '')
+        # app_icon, timeout, ticker are not supported (yet)
+
         notification = NSUserNotification.alloc().init()
-        notification.setTitle_(kwargs.get('title').encode('utf-8'))
-        #notification.setSubtitle_(str(subtitle))
-        notification.setInformativeText_(kwargs.get('message').encode('utf-8'))
-        notification.setSoundName_("NSUserNotificationDefaultSoundName")
-        #notification.setHasActionButton_(False)
-        #notification.setOtherButtonTitle_("View")
-        #notification.setUserInfo_({"action":"open_url", "value":url})
-        NSUserNotificationCenter.defaultUserNotificationCenter() \
-                            .setDelegate_(self)
-        NSUserNotificationCenter.defaultUserNotificationCenter() \
-                            .scheduleNotification_(notification)
+        notification.setTitle_(objc_str(title))
+        notification.setSubtitle_(objc_str(app_name))
+        notification.setInformativeText_(objc_str(message))
+
+        userNotificationCenter = NSUserNotificationCenter.defaultUserNotificationCenter()
+        userNotificationCenter.setDelegate_(self)
+        userNotificationCenter.deliverNotification_(notification)
+
+    @protocol('NSUserNotificationCenterDelegate')
+    def userNotificationCenter_shouldPresentNotification_(self, center, notification):
+        return ObjcBOOL(True)
 
 
 def instance():
     return OSXNotification()
-
