d1a023bee52a6df25bace7770b1a36be141ada3e
kiok46 <kuldeepbb.grewal@gmail.com>
2016-04-17 20:06:55 +0530

    fixes

--- plyer/__init__.py
+++ plyer/__init__.py
@@ -4,8 +4,8 @@ Plyer
 
 '''
 
-__all__ = ('accelerometer', 'audio', 'battery', 'brightness', 'call', 'camera', 'compass',
-           'email', 'filechooser', 'gps', 'gyroscope', 'irblaster',
+__all__ = ('accelerometer', 'audio', 'battery', 'brightness', 'call', 'camera',
+           'compass', 'email', 'filechooser', 'gps', 'gyroscope', 'irblaster',
            'orientation', 'notification', 'sms', 'tts', 'uniqueid', 'vibrator')
 
 __version__ = '1.2.5dev'
