340af85b1cf45dc4e8f9d22dc4b3802d4e769ed1
Tungsteno74 <tungsteno74@yahoo.it>
2016-05-03 05:20:49 +0200

    Update orientation.py

--- plyer/platforms/android/orientation.py
+++ plyer/platforms/android/orientation.py
@@ -42,17 +42,17 @@ class AndroidOrientation(Orientation):
     def _get_orientation(self):
         screenOrientation = activity.getResources().getConfiguration().orientation
         surfaceRotation = activity.getWindowManager().getDefaultDisplay().getRotation()
-        
+
         if surfaceRotation == Surface.ROTATION_0 or surfaceRotation == Surface.ROTATION_90:
-	        if screenOrientation == Configuration.ORIENTATION_PORTRAIT:
-		        return ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
-	        elif screenOrientation == Configuration.ORIENTATION_LANDSCAPE:
-		        return ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+            if screenOrientation == Configuration.ORIENTATION_PORTRAIT:
+                return ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+            elif screenOrientation == Configuration.ORIENTATION_LANDSCAPE:
+                return ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
         elif surfaceRotation == Surface.ROTATION_180 or surfaceRotation == Surface.ROTATION_270:
-	        if screenOrientation == Configuration.ORIENTATION_PORTRAIT:
-		        return ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT
-	        elif screenOrientation == Configuration.ORIENTATION_LANDSCAPE:
-		        return ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE
-		        
+            if screenOrientation == Configuration.ORIENTATION_PORTRAIT:
+                return ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT
+            elif screenOrientation == Configuration.ORIENTATION_LANDSCAPE:
+                return ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE
+
 def instance():
     return AndroidOrientation()
