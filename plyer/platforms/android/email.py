07d31b4f0a2ae54087a91425b9b1cb72dd41bf6f
Ian Giestas Pauli <iangiestaspauli@gmail.com>
2016-05-15 15:19:06 -0300

    Add attachment option
    I was needed that option but it wasn't available... so I coded this one solution...

--- plyer/platforms/android/email.py
+++ plyer/platforms/android/email.py
@@ -4,7 +4,10 @@ from plyer.platforms.android import activity
 
 Intent = autoclass('android.content.Intent')
 AndroidString = autoclass('java.lang.String')
-
+Uri = autoclass('android.net.Uri')
+File = autoclass('java.io.File')
+AndroidString = autoclass('java.lang.String')
+Environment = autoclass('android.os.Environment')
 
 class AndroidEmail(Email):
     def _send(self, **kwargs):
@@ -14,6 +17,7 @@ class AndroidEmail(Email):
         recipient = kwargs.get('recipient')
         subject = kwargs.get('subject')
         text = kwargs.get('text')
+        attachment = kwargs.get('attachment')
         create_chooser = kwargs.get('create_chooser')
 
         if recipient:
@@ -26,7 +30,17 @@ class AndroidEmail(Email):
             android_text = cast('java.lang.CharSequence',
                                 AndroidString(text))
             intent.putExtra(Intent.EXTRA_TEXT, android_text)
-
+        if attachment:
+            """
+            Attachment should be a python list containing two python strings:
+                the first one the path, the second one the filename
+            """
+            path = AndroidString(attachment[0])
+            filename = AndroidString(attachment[1])
+            filelocation = File(path, filename)
+            android_attach = cast('java.lang.CharSequence',
+                                Uri.fromFile(attachment))
+            intent.putExtra(Intent.EXTRA_STREAM, android_attach)
         if create_chooser:
             chooser_title = cast('java.lang.CharSequence',
                                  AndroidString('Send message with:'))
