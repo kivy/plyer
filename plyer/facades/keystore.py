1921f56f52ee717b410e3a3ff22df9de257644e0
Kovak <kovac1066@gmail.com>
2016-05-21 11:12:13 -0600

    fix keying

--- plyer/facades/keystore.py
+++ plyer/facades/keystore.py
@@ -7,13 +7,13 @@ class Keystore(object):
     '''
 
     def set_key(self, servicename, key, value, **kwargs):
-        self._set_password(servicename, key, value, **kwargs)
+        self._set_key(servicename, key, value, **kwargs)
 
     def _set_key(self, servicename, key, value, **kwargs):
         raise NotImplementedError()
 
     def get_key(self, servicename, key, **kwargs):
-        return self._get_password(servicename, username)
+        return self._get_key(servicename, key)
 
     def _get_key(self, servicename, key, **kwargs):
         raise NotImplementedError()
