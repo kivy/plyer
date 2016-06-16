eca2ea149714a25162b67f73cbe2aae44f1c6468
kiok46 <kuldeepbb.grewal@gmail.com>
2016-06-17 02:14:28 +0530

    add records

--- plyer/facades/nfc.py
+++ plyer/facades/nfc.py
@@ -1,34 +1,191 @@
+class PyNdefRecord(object):
+    '''
+    Python implemetation of andriod NDefRecords.
+    '''
+
+    def create_application_record(self, payload):
+        '''
+        Create a new application record.
+        '''
+        self._create_application_record(payload=payload)
+
+    def create_external(self, payload):
+        '''
+        To encode application specific data into NdefRecord.
+        '''
+        self._create_external(payload=payload)
+
+    def create_mime(self, payload):
+        '''
+        To encode MIME type data into NdefRecord like, text/plain,
+        image/jpeg, */*. etc
+        '''
+        self._create_mime(payload=payload)
+
+    def create_text_record(self, payload):
+        '''
+        To create NdefRecord containing UTF-8 text data.
+        '''
+        self._create_text_record(payload=payload)
+
+    def create_uri(self, payload):
+        '''
+        To encode a URI in NdefRecord.
+        '''
+        self._create_uri(payload=payload)
+
+    def create_RTD_ALTERNATIVE_CARRIER(self, payload):
+        '''
+        RTD Alternative Carrier type. For use with TNF_WELL_KNOWN.
+        '''
+        self._create_RTD_ALTERNATIVE_CARRIER(payload=payload)
+
+    def create_RTD_HANDOVER_CARRIER(self, payload):
+        '''
+        RTD Handover Carrier type. For use with TNF_WELL_KNOWN.
+        '''
+        self._create_RTD_HANDOVER_CARRIER(payload=payload)
+
+    def create_RTD_HANDOVER_REQUEST(self, payload):
+        '''
+        RTD Handover Request type. For use with TNF_WELL_KNOWN.
+        '''
+        self._create_RTD_HANDOVER_REQUEST(payload=payload)
+
+    def create_RTD_HANDOVER_SELECT(self, payload):
+        '''
+        RTD Handover Select type. For use with TNF_WELL_KNOWN.
+        '''
+        self._create_RTD_HANDOVER_SELECT(payload=payload)
+
+    def create_RTD_SMART_POSTER(self, payload):
+        '''
+        RTD Smart Poster type. For use with TNF_WELL_KNOWN.
+        '''
+        self._create_RTD_SMART_POSTER(payload=payload)
+
+    def create_RTD_TEXT(self, payload):
+        '''
+        RTD Text type. For use with TNF_WELL_KNOWN.
+        '''
+        self._create_RTD_TEXT(payload=payload)
+
+    # private
+
+    def _create_application_record(self, **kwargs):
+        raise NotImplementedError()
+
+    def _create_external(self, **kwargs):
+        raise NotImplementedError()
+
+    def _create_mime(self, **kwargs):
+        raise NotImplementedError()
+
+    def _create_text_record(self, **kwargs):
+        raise NotImplementedError()
+
+    def _create_uri(self, **kwargs):
+        raise NotImplementedError()
+
+    def _create_RTD_ALTERNATIVE_CARRIER(self, **kwargs):
+        raise NotImplementedError()
+
+    def _create_RTD_HANDOVER_CARRIER(self, **kwargs):
+        raise NotImplementedError()
+
+    def _create_RTD_HANDOVER_REQUEST(self, **kwargs):
+        raise NotImplementedError()
+
+    def _create_RTD_HANDOVER_SELECT(self, **kwargs):
+        raise NotImplementedError()
+
+    def _create_RTD_SMART_POSTER(self, **kwargs):
+        raise NotImplementedError()
+
+    def _create_RTD_TEXT(self, **kwargs):
+        raise NotImplementedError()
+
+
 class NFC(object):
     ''' NFC facade.
     .. note::
         On Android your app needs the NFC permission.
     '''
-
-    def read_tag(self):
-        self._read_tag()
-
-    def write_tag(self):
-        self._write_tag()
-
-    def nfc_enable_ndef_exchange(self):
-        self._nfc_enable_ndef_exchange()
-
-    def nfc_disable_ndef_exchange(self):
-        self._nfc_disable_ndef_exchange()
-
-    def on_pause(self):
-        self._on_pause()
-
-    def on_resume(self):
-        self._on_resume()
+    NdefRecord = PyNdefRecord
 
     def enable(self):
+        '''
+        To enable NFC.
+        '''
         self._enable()
 
     def disable(self):
+        '''
+        to disable NFC.
+        '''
         self._disable()
 
+    def write_record(self, ):
+        '''
+        Write Android NDefRecord.
+        '''
+        self._write_record()
+
+    def read_record(self, ndef_type, payload):
+        '''
+        Read Android NdefRecord.
+        '''
+        self._read_records()
+
+    def read_tag(self):
+        '''
+        To read the tags detected by NFC.
+        '''
+        self._read_tag()
+
+    def write_tag(self):
+        '''
+        To write on tags detected by NFC.
+        '''
+        self._write_tag()
+
+    def nfc_enable_exchange(self):
+        '''
+        Enable ndef exchange.
+        '''
+        self._nfc_enable_exchange()
+
+    def nfc_disable_exchange(self):
+        '''
+        Disable ndef exchange
+        '''
+        self._nfc_disable_exchange()
+
+    def on_pause(self):
+        '''
+        Events called when application is paused.
+        '''
+        self._on_pause()
+
+    def on_resume(self):
+        '''
+        Event called when application is resumed.
+        '''
+        self._on_resume()
+
     # private
+
+    def _enable(self, **kwargs):
+        raise NotImplementedError()
+
+    def _diable(self, **kwargs):
+        raise NotImplementedError()
+
+    def _write_record(self, **kwargs):
+        raise NotImplementedError()
+
+    def _read_record(self, **kwargs):
+        raise NotImplementedError()
 
     def _read_tag(self, **kwargs):
         raise NotImplementedError()
@@ -36,20 +193,14 @@ class NFC(object):
     def _write_tag(self, **kwargs):
         raise NotImplementedError()
 
-    def _nfc_enable_ndef_exchange(self, **kwargs):
+    def _nfc_enable_exchange(self, **kwargs):
         raise NotImplementedError()
 
-    def _nfc_disable_ndef_exchange(self, **kwargs):
+    def _nfc_disable_exchange(self, **kwargs):
         raise NotImplementedError()
 
     def _on_pause(self, **kwargs):
         raise NotImplementedError()
-    
+
     def _on_resume(self, **kwargs):
-        raise NotImplementedError()
-
-    def _enable(self, **kwargs):
-        raise NotImplementedError()
-
-    def _diable(self, **kwargs):
         raise NotImplementedError()
--- plyer/platforms/android/nfc.py
+++ plyer/platforms/android/nfc.py
@@ -8,6 +8,8 @@ PythonActivity = autoclass('org.renpy.android.PythonActivity')
 Intent = autoclass('android.content.Intent')
 IntentFilter = autoclass('android.content.IntentFilter')
 PendingIntent = autoclass('android.app.PendingIntent')
+JavaString = autoclass('java.lang.String')
+BUILDVERSION = autoclass('android.os.Build$VERSION').SDK_INT
 NdefRecord = autoclass('android.nfc.NdefRecord')
 # immutable NDEF Record.
 NdefMessage = autoclass('android.nfc.NdefMessage')
@@ -33,26 +35,273 @@ NfcEvent = autoclass('android.nfc.NfcEvent')
 # Information associated with NFC events
 
 
+class PyNdefRecord(NFC.NdefRecord):
+
+    def __init__(self, **kwargs):
+        pass
+        # self.ndef_type = ndef_type
+        # self.payload = payload
+        # if self.ndef_type == 'application':
+        #     return self._create_application_record(payload)
+
+    def __call__(self, **kwargs):
+
+        payload = kwargs.get("payload")
+        ndef_type = kwargs.get("ndef_type")
+        if ndef_type == "application":
+            return self._create_application_record(payload)
+        elif ndef_type == "external":
+            return self._create_external(payload)
+        elif ndef_type == "mime":
+            return self._create_mime(payload)
+        elif ndef_type == "text_record":
+            return self._create_text_record(payload)
+        elif ndef_type == "uri":
+            return self._create_uri(payload)
+        elif ndef_type == "alternative_carrier":
+            return self._create_RTD_ALTERNATIVE_CARRIER(payload)
+        elif ndef_type == "handover_carrier":
+            return self._create_uri(payload)
+        elif ndef_type == "handover_request":
+            return self._create_uri(payload)
+        elif ndef_type == "handover_select":
+            return self._create_uri(payload)
+        elif ndef_type == "smart_poster":
+            return self._create_uri(payload)
+        elif ndef_type == "text":
+            return self._create_RTD_TEXT(payload)
+        else:
+            return
+
+    def _create_application_record(self, **kwargs):
+        payload = kwargs.get('payload')
+        package_name = payload.get('package_name')
+        return NdefRecord.createApplicationRecord(JavaString(package_name))
+
+    def _create_external(self, **kwargs):
+        payload = kwargs.get('payload')
+        domain = payload.get('domain')
+        external_type = payload.get('external_type')
+        data = kwargs.get('data')
+        if BUILDVERSION > 16:
+            return NdefRecord.createExternal(JavaString(domain),
+                                             JavaString(external_type),
+                                             data)
+        record = NdefRecord(NdefRecord.TNF_EXTERNAL_TYPE,
+                            "{} + ':' + {}".format(JavaString(domain),
+                                                   JavaString(external_type)),
+                            '',
+                            payload)
+        return record
+
+    def _create_mime(self, **kwargs):
+        payload = kwargs.get('payload')
+        mime_type = payload.get('mime_type')
+        mime_data = payload.get('mime_data')
+        if BUILDVERSION > 16:
+            return NdefRecord.createMime(JavaString(mime_type),
+                                         mime_data)
+        record = NdefRecord(NdefRecord.TNF_MIME_MEDIA,
+                            JavaString(mime_type),
+                            '',
+                            payload)
+        return record
+
+    def _create_text_record(self, **kwargs):
+        payload = kwargs.get('payload')
+        language_code = payload.get('language_code')
+        text = kwargs.get('text')
+        if BUILDVERSION > 21:
+            return NdefRecord.createTextRecord(JavaString(language_code),
+                                               JavaString(text))
+        record = NdefRecord(NdefRecord.TNF_WELL_KNOWN,
+                            language_code,
+                            '',
+                            text)
+        return record
+
+    def _create_uri(self, **kwargs):
+        payload = kwargs.get('payload')
+        uri = payload.get('uri')
+        if BUILDVERSION > 14:
+            return NdefRecord.createUri(JavaString(uri))
+
+        record = NdefRecord(NdefRecord.TNF_WELL_KNOWN,
+                            NdefRecord.RTD_URI,
+                            '',
+                            uri)
+        return record
+
+    def _create_RTD_ALTERNATIVE_CARRIER(self, **kwargs):
+        payload = kwargs.get('payload')
+        data = payload.get('alterative_carrier')
+        record = NdefRecord(NdefRecord.TNF_WELL_KNOWN,
+                            NdefRecord.RTD_ALTERNATIVE_CARRIER,
+                            '',
+                            data)
+        return record
+
+    def _create_RTD_HANDOVER_CARRIER(self, **kwargs):
+        payload = kwargs.get('payload')
+        data = payload.get('handover_carrier')
+        record = NdefRecord(NdefRecord.TNF_WELL_KNOWN,
+                            NdefRecord.RTD_HANDOVER_CARRIER,
+                            '',
+                            data)
+        return record
+
+    def _create_RTD_HANDOVER_REQUEST(self, **kwargs):
+        payload = kwargs.get('payload')
+        data = payload.get('handover_request')
+        record = NdefRecord(NdefRecord.TNF_WELL_KNOWN,
+                            NdefRecord.RTD_HANDOVER_REQUEST,
+                            '',
+                            data)
+        return record
+
+    def _create_RTD_HANDOVER_SELECT(self, **kwargs):
+        payload = kwargs.get('payload')
+        data = payload.get('handover_select')
+        record = NdefRecord(NdefRecord.TNF_WELL_KNOWN,
+                            NdefRecord.RTD_HANDOVER_SELECT,
+                            '',
+                            data)
+        return record
+
+    def _create_RTD_SMART_POSTER(self, **kwargs):
+        payload = kwargs.get('payload')
+        data = payload.get('smart_poster')
+        record = NdefRecord(NdefRecord.TNF_WELL_KNOWN,
+                            NdefRecord.RTD_SMART_POSTER,
+                            '',
+                            data)
+        return record
+
+    def _create_RTD_TEXT(self, **kwargs):
+        payload = kwargs.get('payload')
+        data = payload.get('text')
+        record = NdefRecord(NdefRecord.TNF_WELL_KNOWN,
+                            NdefRecord.RTD_TEXT,
+                            '',
+                            data)
+        return record
+
+
 class AndroidNFC(NFC):
 
     tag_message = ''
 
-    ''' method adapted from `https://gist.github.com/tito/9e2308a4c942ddb2342b` '''
+    '''
+    method adapted from `https://gist.github.com/tito/9e2308a4c942ddb2342b`
+    and
+    `https://gist.github.com/akshayaurora/b76dbcc69c9850bb3e318af4722a0b4f`
+    '''
     def nfc_init(self):
+        '''
+        Initializing NFC adapter and listing the techs available.
+        '''
 
         self.j_context = context = PythonActivity.mActivity
         self.nfc_adapter = NfcAdapter.getDefaultAdapter(context)
+        if not nfc_adapter:
+            return
         self.nfc_pending_intent = PendingIntent.getActivity(context, 0,
-                                Intent(context, context.getClass()).addFlags(
-                                       Intent.FLAG_ACTIVITY_SINGLE_TOP), 0)
+                                  Intent(context, context.getClass()).addFlags(
+                                         Intent.FLAG_ACTIVITY_SINGLE_TOP), 0)
 
-        print 'p2p filter'
         self.ndef_detected = IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED)
-        self.ndef_detected.addDataType('text/plain')
-        self.ndef_exchange_filters = [self.ndef_detected]
+        self.tech_detected = IntentFilter(NfcAdapter.ACTION_TECH_DISCOVERED)
+        self.tag_detected = IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED)
 
-    ''' some methods based on android documentation
-        https://developer.android.com/guide/topics/connectivity/nfc/nfc.html'''
+        self.ndef_detected.addDataType('*/*')
+        self.tech_detected.addDataType('*/*')
+        self.tag_detected.addDataType('*/*')
+
+        self.ndef_tech_list = [
+            ['android.nfc.tech.IsoDep'],
+            ['android.nfc.tech.NfcA'],
+            ['android.nfc.tech.NfcB'],
+            ['android.nfc.tech.NfcF'],
+            ['android.nfc.tech.NfcV'],
+            ['android.nfc.tech.Ndef'],
+            ['android.nfc.tech.NdefFormattable'],
+            ['android.nfc.tech.MifareClassic']
+            ['android.nfc.tech.MifareUltralight'],
+            ]
+
+        self.ndef_exchange_filters = [
+            self.ndef_detected,
+            self.tech_detected,
+            self.tag_detected
+            ]
+
+        self._enable()
+        self._on_new_intent(PythonActivity.getIntent())
+        return True
+
+    def _disable(self):
+        activity.unbind(on_new_intent=self._on_new_intent)
+        self._disable_foreground_dispatch()
+
+    def _enable(self):
+        activity.bind(on_new_intent=self._on_new_intent)
+        self._enable_foreground_dispatch()
+
+    # Returns android NdefRecord object.
+    def _write_record(self, ndef_type, payload):
+        '''
+        For writing android NdefRecords.
+        '''
+        record = PyNdefRecord()
+        return record(ndef_type, payload)
+
+    def _read_record(self):
+        '''
+        Reading android NdefRecords.
+        '''
+        return
+
+    '''
+    method adapted from `https://gist.github.com/tito/9e2308a4c942ddb2342b`
+    '''
+    @run_on_ui_thread
+    def _nfc_enable_ndef_exchange(self, **kwargs):
+        ndef_record = NdefRecord(
+                NdefRecord.TNF_MIME_MEDIA,
+                'text/plain', '', 'hello world')
+        ndef_message = NdefMessage([ndef_record])
+
+        self.nfc_adapter.enableForegroundNdefPush(self.j_context, ndef_message)
+
+        self.nfc_adapter.enableForegroundDispatch(self.j_context,
+                self.nfc_pending_intent, self.ndef_exchange_filters, [])
+
+    '''
+    method adapted from `https://gist.github.com/tito/9e2308a4c942ddb2342b`
+    '''
+    @run_on_ui_thread
+    def _nfc_disable_ndef_exchange(self):
+        self.nfc_adapter.disableForegroundNdefPush(self.j_context)
+        self.nfc_adapter.disableForegroundDispatch(self.j_context)
+
+    @run_on_ui_thread
+    def _disable_foreground_dispatch(self):
+        self.nfc_adapter.disableForegroundDispatch(self.j_context)
+
+    @run_on_ui_thread
+    def _enable_foreground_dispatch(self):
+        self.nfc_adapter.enableForegroundDispatch(self.j_context,
+                                                  self.nfc_pending_intent,
+                                                  self.ndef_exchange_filters,
+                                                  self.ndef_tech_list)
+
+    def _nfc_enable_exchange(self, **kwargs):
+        data = kwargs.get('data')
+        self._nfc_enable_ndef_exchange(data)
+
+    def _nfc_disable_exchange(self):
+        self._nfc_disable_ndef_exchange()
+
     def _on_new_intent(self, intent):
         if intent.getAction() == NfcAdapter.ACTION_NDEF_DISCOVERED:
 
@@ -70,29 +319,32 @@ class AndroidNFC(NFC):
             # Tag object representing the scanned tag.
             extra_ID = intent.getParcelableExtra(NfcAdapter.EXTRA_ID)
             # Low Level ID of the tag
-            return {'payload': '{}'.format(''.join(map(chr, payload)),
+            return {'payload': {}.format(''.join(map(chr, payload))),
                     'tag': extra_tag,
                     'id': extra_ID}
 
         elif intent.getAction() == NfcAdapter.ACTION_TECH_DISCOVERED:
-            pass
+            return
 
         elif intent.getAction() == NfcAdapter.ACTION_TAG_DISCOVERED:
             # If tag is discovered.
             self._get_tag_info(intent)
 
     def _get_tag_info(self, intent):
+        '''
+        Reading the tags of the type listed in the ndef_tech_list.
+        '''
 
-        tag=intent.getParcableExtra(NfcAdapter.EXTRA_TAG)
-        taglist=tag.getTechList()
+        tag = intent.getParcableExtra(NfcAdapter.EXTRA_TAG)
+        taglist = tag.getTechList()
 
         for i in range(len(taglist)):
             if taglist[i] == MifareClassic.class.getName():
-                mctag=MifareClassic.get(tag)
-                mctag_type=mctag.getType()
+                mctag = MifareClassic.get(tag)
+                mctag_type = mctag.getType()
 
                 if (mctag_type == MifareClassic.TYPE_CLASSIC):
-                    mfc=MifareClassic.get(tag)
+                    mfc = MifareClassic.get(tag)
                     pass
 
                 elif (mctag_type == MifareClassic.TYPE_PLUS):
@@ -102,7 +354,7 @@ class AndroidNFC(NFC):
                     pass
 
             elif taglist[i] == MifareUltralight.class.getName():
-                mutag_type=mutag.getType()
+                mutag_type = mutag.getType()
 
                 if (mutag_type == MifareUltralight.TYPE_ULTRALIGHT):
                     self._read_ultra_tag(tag)
@@ -111,14 +363,49 @@ class AndroidNFC(NFC):
                     pass
 
             elif taglist[i] == IsoDep.class.getName():
-                pass
+                self._read_isodep_rag(tag)
 
             elif taglist[i] == Ndef.class.getName():
                 self._read_ndef_tag(tag)
 
             elif taglist[i] == NdefFormatable.class.getName():
-                pass
+                self._read_ndef_formattable_tag(tag)
 
+    def _read_classic_tag(self, Tag):
+        return
+
+    def _read_ultra_tag(self, Tag):
+        mifare = MifareUltralight.get(tag)
+
+        try:
+            mifare.connect()
+            payload = mifare.readPages(4)
+            self.tag_message = str(payload, Charset.forName("US-ASCII"))
+            return str(payload, Charset.forName("US-ASCII"))
+
+        except:
+            return
+
+        finally:
+            mifare.close()
+
+    def _read_iso_dep_tag(self):
+        return
+
+    def _read_ndef_tag(self, Tag):
+        return
+
+    def _read_iso_dep_tag(self, Tag):
+        return
+
+    def _read_ndef_formattable_tag(self, Tag):
+        return
+
+    def write_tag(self, Tag, tagText):
+        return
+
+    def _write_classic_tag(self, Tag, tagText):
+        return
 
     def _write_ultra_tag(self, Tag, tagText):
         ''' MifareUltralight tags contains 16 pages and each page contains
@@ -127,7 +414,7 @@ class AndroidNFC(NFC):
         Page data must be equal 4 bytes and page number must be less than 16.
         '''
 
-        ultralight=MifareUltralight.get(Tag)
+        ultralight = MifareUltralight.get(Tag)
         try:
             ultralight.connect()
             ultralight.writePage(4, tagText.encode())
@@ -140,60 +427,21 @@ class AndroidNFC(NFC):
 
     def _write_ndef_tag(self, Tag, tagText):
 
-        ndef_record=NdefRecord(
+        ndef_record = NdefRecord(
                 NdefRecord.TNF_WELL_KNOWN,
                 NdefRecord.RTD_TEXT,
                 tagText)
-        ndef_message=NdefMessage([ndef_record])
-        ndef=Ndef.get(Tag)
+        ndef_message = NdefMessage([ndef_record])
+        ndef = Ndef.get(Tag)
         ndef.connect()
         ndef.writeNdefMessage(tagText)
         ndef.close()
 
-    def _read_ultra_tag(self, Tag):
-        mifare=MifareUltralight.get(tag)
+    def _write_isodep_tag(self, Tag, tagText):
+        return
 
-        try:
-            mifare.connect()
-            payload=mifare.readPages(4)
-            self.tag_message=str(payload, Charset.forName("US-ASCII"))
-            return str(payload, Charset.forName("US-ASCII"))
-
-        except:
-            return
-
-        finally:
-            mifare.close()
-
-
-    def _read_ndef_tag(self, Tag):
-        pass
-
-    def _disable(self):
-        activity.unbind(on_new_intent=self._on_new_intent)
-
-    def _enable(self):
-        activity.bind(on_new_intent=self._on_new_intent)
-
-
-    ''' method adapted from `https://gist.github.com/tito/9e2308a4c942ddb2342b` '''
-    @run_on_ui_thread
-    def _nfc_enable_ndef_exchange(self, **kwargs):
-        ndef_record=NdefRecord(
-                NdefRecord.TNF_MIME_MEDIA,
-                'text/plain', '', 'hello world')
-        ndef_message=NdefMessage([ndef_record])
-
-        self.nfc_adapter.enableForegroundNdefPush(self.j_context, ndef_message)
-
-        self.nfc_adapter.enableForegroundDispatch(self.j_context,
-                self.nfc_pending_intent, self.ndef_exchange_filters, [])
-
-    ''' method adapted from `https://gist.github.com/tito/9e2308a4c942ddb2342b` '''
-    @run_on_ui_thread
-    def _nfc_disable_ndef_exchange(self):
-        self.nfc_adapter.disableForegroundNdefPush(self.j_context)
-        self.nfc_adapter.disableForegroundDispatch(self.j_context)
+    def _write_ndef_formattable_tag(self, Tag, tagText):
+        return
 
     def _on_pause(self):
         self._disable()
