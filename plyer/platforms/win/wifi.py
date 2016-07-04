366db4b653f570c0693a0870a93413b9e07ea59d
kiok46 <kuldeepbb.grewal@gmail.com>
2016-07-04 20:08:24 +0530

    add windows

---
+++ plyer/platforms/win/wifi.py
@@ -0,0 +1,69 @@
+import plyer.platforms.win.libs.wifi_defs as wifi_lib
+#import libs.wifi_defs as wifi_lib
+from plyer.facades import Wifi
+
+class WindowWifi(Wifi):
+
+    names = {}
+
+    def _is_enabled(self):
+        '''
+        Returns `True`if the Wifi is enables else `False`.
+        '''
+        return True
+        #return wifi_lib.is_enabled()
+
+    def _enable(self):
+        '''
+        Method to turn on the Wi-Fi.
+        '''
+        wifi_lib.enable()
+
+    def _disable(self):
+        '''
+        Method to turn off the Wi-Fi.
+        '''
+        wifi_lib.disable()
+
+    def _get_network_info(self, name):
+        '''
+        Returns all the network information.
+        '''
+        return wifi_lib.get_network_info(name)
+
+    def _start_scanning(self):
+        '''
+        Starts scanning for available Wi-Fi networks and returns the available,
+        devices.
+        '''
+        self.names = wifi_lib.start_scanning()
+
+    def _stop_scanning(self):
+        '''
+        Can't stop scanning, API not provided by Windows.
+        '''
+        wifi_lib.stop_scanning()
+
+    def _get_available_wifi(self):
+        '''
+        Returns the name of available networks.
+        '''
+        return wifi_lib.get_available_wifi()
+
+    def _connect(self, network_name, password):
+        '''
+        Expects 2 parameters:
+            - name/ssid of the network.
+            - password
+        '''
+        wifi_lib.connect(network_name, password)
+
+    def _disconnect(self):
+        '''
+        Disconnect from network.
+        '''
+        wifi_lib.disconnect()
+
+
+def instance():
+    return WindowWifi()
