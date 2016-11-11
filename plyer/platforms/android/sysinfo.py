566ab13c1301155af1137eaef52fc28fced6e98b
Valentine Davydov <42@doratoa.net>
2016-11-11 19:03:23 +0300

    android fixes

--- plyer/platforms/android/sysinfo.py
+++ plyer/platforms/android/sysinfo.py
@@ -1,13 +1,15 @@
 from jnius import autoclass
 from plyer.facades import Sysinfo
+from plyer.platforms.android import activity
 
 Build = autoclass('android.os.Build')
 BuildVersion = autoclass('android.os.Build$VERSION')
 BuildVersionCodes = autoclass('android.os.Build$VERSION_CODES')
 System = autoclass('java.lang.System')
 Environment = autoclass('android.os.Environment')
-StatsFs = autoclass('android.os.StatsFs')
+StatsFs = autoclass('android.os.StatFs')
 DisplayMetrics = autoclass('android.util.DisplayMetrics')
+Context = autoclass('android.content.Context')
 
 
 class AndroidSysinfo(Sysinfo):
@@ -43,14 +45,17 @@ class AndroidSysinfo(Sysinfo):
         sdkint = BuildVersion.SDK_INT
         version = BuildVersion.RELEASE
         try:
-            fields = BuildVersionCodes.class.getFields()
+            fields = BuildVersionCodes.getClass().getFields()
             for field in fields:
                 fieldName = field.getName()
                 fieldValue = field.getInt()
 
                 if (fieldValue == sdkint):
                     return ('Android', str(sdkint), fieldName)
-        except:
+        except Exception as ex:
+            # WARNING TEST
+            # from kivy.logger import Logger
+            # Logger.error('plyer: could not get version code: {0}'.format(ex))
             return ('Android', str(sdkint), "UNKNOWN")
 
     def _architecture_info(self):
@@ -91,7 +96,9 @@ class AndroidSysinfo(Sysinfo):
         Returns the screen resolution for example:
         '''
         dm = DisplayMetrics()
-        getWindowManager.getDefaultDisplay().getMetrics(dm)
+        context = activity.getApplicationContext()
+        wm = context.getSystemService(Context.WINDOW_SERVICE)
+        wm.getDefaultDisplay().getMetrics(dm)
         wid = int(dm.widthPixels)
         hei = int(dm.heightPixels)
         return (wid, hei)
