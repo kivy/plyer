249363aed1f149112fc48c4b25d6d4ed6bcd50df
Valentine Davydov <42@doratoa.net>
2016-11-11 19:13:50 +0300

    example strings and notes for android

--- plyer/platforms/android/sysinfo.py
+++ plyer/platforms/android/sysinfo.py
@@ -16,25 +16,26 @@ class AndroidSysinfo(Sysinfo):
 
     def _model_info(self):
         '''
-        Returns the model info for example:
+        Returns the model info for example: ASUS_Z00ED
+        Examples are given for Asus Zenphone 2 laser 5"
         '''
         return Build.MODEL
 
     def _system_name(self):
         '''
-        Returns the system's OS name for example:
+        Returns the system's OS name for example: Linux
         '''
         return System.getProperty("os.name")
 
     def _platform_info(self):
         '''
-        Returns platform's name for example:
+        Returns platform's name for example: ASUS_Z00E_2
         '''
         return Build.DEVICE
 
     def _processor_info(self):
         '''
-        Returns the type of processor for example:
+        Returns the type of processor for example: armeabi-v7a
         '''
         return Build.CPU_ABI
 
@@ -60,31 +61,35 @@ class AndroidSysinfo(Sysinfo):
 
     def _architecture_info(self):
         '''
-        Returns the architecture in a tuple for example:
+        Returns the architecture in a tuple,
+        for example: ('armeabi-v7a', 'armeabi')
+
         '''
         return (Build.CPU_ABI, Build.CPU_ABI2)
 
     def _device_name(self):
         '''
-        Returns the device name for example:
+        Returns the device name for example: asus ASUS_Z00ED
         '''
         return str(Build.MANUFACTURER) + " " + str(Build.MODEL)
 
     def _manufacturer_name(self):
         '''
-        Returns the manufacturer's name for example:
+        Returns the manufacturer's name for example: asus
         '''
         return Build.MANUFACTURER
 
     def _kernel_version(self):
         '''
-        Returns the kernel version for example:
+        Returns the kernel version for example: 3.10.49-perf-g4186cc1
         '''
         return System.getProperty("os.version")
 
     def _storage_info(self):
         '''
         Returns the amount of storage (RAM) in GB. for example:
+        NOTE WARNING this doesn't seem to return GBs of RAM.
+        My phone reports 10610. Fuyou.
         '''
         stat = StatsFs(Environment.getDataDirectory().getPath())
         bytesAvailable = stat.getBlockSize() * stat.getBlockCount()
@@ -93,7 +98,7 @@ class AndroidSysinfo(Sysinfo):
 
     def _screen_resolution(self):
         '''
-        Returns the screen resolution for example:
+        Returns the screen resolution as list, for example: [720,1280]
         '''
         dm = DisplayMetrics()
         context = activity.getApplicationContext()
