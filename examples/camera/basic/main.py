094125d442e47ec026b9b65186746884cb97a614
dessant <git@rmn.space>
2016-12-18 11:18:16 +0200

    pass popup message as positional arg in camera example

--- examples/camera/basic/main.py
+++ examples/camera/basic/main.py
@@ -31,7 +31,7 @@ class CameraDemo(FloatLayout):
         ext = splitext(filepath)[-1].lower()
 
         if(exists(filepath)):
-            popup = MsgPopup(msg="Picture with this name already exists!")
+            popup = MsgPopup("Picture with this name already exists!")
             popup.open()
             return False
 
@@ -39,16 +39,16 @@ class CameraDemo(FloatLayout):
             camera.take_picture(filename=filepath,
                                 on_complete=self.camera_callback)
         except NotImplementedError:
-            popup = MsgPopup(msg="This feature has not yet been implemented for\
-                                this platform.")
+            popup = MsgPopup(
+                "This feature has not yet been implemented for this platform.")
             popup.open()
 
     def camera_callback(self, filepath):
         if(exists(filepath)):
-            popup = MsgPopup(msg="Picture saved!")
+            popup = MsgPopup("Picture saved!")
             popup.open()
         else:
-            popup = MsgPopup(msg="Could not save your picture!")
+            popup = MsgPopup("Could not save your picture!")
             popup.open()
 
 
