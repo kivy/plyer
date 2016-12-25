c861a80f97b91e8e5f9e4333bd3633421f39fc7d
phunsukwangdu <susmit9370@gmail.com>
2016-12-25 20:28:19 +0530

    inapp browsing for android and ios

---
+++ examples/inappbrowser/main.py
@@ -0,0 +1,48 @@
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import StringProperty
+from plyer import inappbrowser
+
+Builder.load_string('''
+<InAppBrowserInterface>:
+    orientation: 'vertical'
+    Label:
+        id: url_label
+        text: 'Please input URL to start'
+    TextInput:
+        id: entered_url
+        text: 'https://kivy.org/#home'
+    URLfireButton:
+        url_input: entered_url.text
+        text: 'Enter'
+        size_hint_y: None
+        on_release: self.open_url()
+''')
+
+
+class InAppBrowserInterface(BoxLayout):
+    pass
+
+
+class URLfireButton(Button):
+    url_input = StringProperty()
+
+    def open_url(self, *args):
+        inappbrowser.access_url(url=self.url_input)
+
+
+class InAppBrowserSampleApp(App):
+
+    def build(self):
+        return InAppBrowserInterface()
+
+    def on_pause(self):
+        return True
+
+    def on_resume(self):
+        pass
+
+if __name__ == "__main__":
+    InAppBrowserSampleApp().run()
