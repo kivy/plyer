5a7e7c3efeb3e65dcd307481ac7691ec8b2da032
phunsukwangdu <susmit9370@gmail.com>
2017-01-27 01:19:41 +0530

    pep8 fixes

--- plyer/platforms/linux/orientation.py
+++ plyer/platforms/linux/orientation.py
@@ -7,17 +7,17 @@ class AndroidOrientation(Orientation):
     def _set_landscape(self, **kwargs):
         self.rotate = 'normal'
         self.screen = sb.check_output("xrandr -q | grep ' connected' \
-                              |  head -n 1 | cut -d ' ' -f1", shell = True)
+                              |  head -n 1 | cut -d ' ' -f1", shell=True)
         self.screen = self.screen.split('\n')[0]
         sb.call(["xrandr", "--output", self.screen, "--rotate", self.rotate])
 
     def _set_portrait(self, **kwargs):
         self.rotate = 'left'
         self.screen = sb.check_output("xrandr -q | grep ' connected' \
-                              |  head -n 1 | cut -d ' ' -f1", shell = True)
+                              |  head -n 1 | cut -d ' ' -f1", shell=True)
         self.screen = self.screen.split('\n')[0]
         sb.call(["xrandr", "--output", self.screen, "--rotate", self.rotate])
-        
+
 
 def instance():
     return AndroidOrientation()
