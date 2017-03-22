c9741918be8c8bc74e6016a4c60c42a5e30aa88a
malverick <sumitlnmiit96@gmail.com>
2017-03-22 23:36:55 +0530

    Add android implementation for gyroscope uncalibrated

--- plyer/platforms/android/gyroscope.py
+++ plyer/platforms/android/gyroscope.py
@@ -41,6 +41,35 @@ class GyroscopeSensorListener(PythonJavaClass):
         pass
 
 
+class GyroUncalibratedSensorListener(PythonJavaClass):
+    __javainterfaces__ = ['android/hardware/SensorEventListener']
+
+    def __init__(self):
+        super(GyroUncalibratedSensorListener, self).__init__()
+        service = activity.getSystemService(Context.SENSOR_SERVICE)
+        self.SensorManager = cast('android.hardware.SensorManager', service)
+
+        self.sensor = self.SensorManager.getDefaultSensor(
+            Sensor.TYPE_GYROSCOPE_UNCALIBRATED)
+        self.values = None
+
+    def enable(self):
+        self.SensorManager.registerListener(self, self.sensor,
+                    SensorManager.SENSOR_DELAY_NORMAL)
+
+    def disable(self):
+        self.SensorManager.unregisterListener(self, self.sensor)
+
+    @java_method('(Landroid/hardware/SensorEvent;)V')
+    def onSensorChanged(self, event):
+        self.values = event.values[:6]
+
+    @java_method('(Landroid/hardware/Sensor;I)V')
+    def onAccuracyChanged(self, sensor, accuracy):
+        pass
+
+
+
 class AndroidGyroscope(Gyroscope):
     def __init__(self):
         super(AndroidGyroscope, self).__init__()
@@ -48,21 +77,31 @@ class AndroidGyroscope(Gyroscope):
 
     def _enable(self):
         if (not self.bState):
-            self.listener = GyroscopeSensorListener()
-            self.listener.enable()
+            self.listenerg = GyroscopeSensorListener()
+            self.listenergu = GyroUncalibratedSensorListener()
+            self.listenerg.enable()
+            self.listenergu.enable()
             self.bState = True
 
     def _disable(self):
         if (self.bState):
             self.bState = False
-            self.listener.disable()
-            del self.listener
+            self.listenerg.disable()
+            self.listenergu.disable()
+            del self.listenerg
+            del self.listenergu
 
-    def _get_orientation(self):
+    def _get_rotation(self):
         if (self.bState):
-            return tuple(self.listener.values)
+            return tuple(self.listenerg.values)
         else:
             return (None, None, None)
+
+    def _get_rotationUncalib(self):
+        if (self.bState):
+            return tuple(self.listenergu.values)
+        else:
+            return (None, None, None, None, None, None)
 
     def __del__(self):
         if(self.bState):
