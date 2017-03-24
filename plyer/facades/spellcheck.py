448fa271593f1a9ad4c61c092c00c7138be595f9
malverick <sumitlnmiit96@gmail.com>
2017-03-24 17:14:51 +0530

    Add spellcheck facade

---
+++ plyer/facades/spellcheck.py
@@ -0,0 +1,17 @@
+class SpellCheck(object):
+    '''
+    Documentation to be added
+
+    .. versionadded:: 1.3.1
+    '''
+
+    def get_suggestions(self, text):
+        '''
+
+        '''
+        return self._get_suggestions(text)
+
+    #private
+
+    def _get_suggestions(self, text):
+        raise NotImplementedError()
