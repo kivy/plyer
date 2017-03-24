b05b3ecd1af54072d866ebeb53139cfde7d7d1b6
bhaveshAn <bhaveshanand7@gmail.com>
2017-03-25 02:37:18 +0530

    gstreamer command change

--- plyer/platforms/linux/camera.py
+++ plyer/platforms/linux/camera.py
@@ -5,23 +5,22 @@ from plyer.facades import Camera
 class LinuxCamera(Camera):
 
     def _take_picture(self, on_complete, filename=None):
-        assert(on_complete is not None)
         self.on_complete = on_complete
         self.filename = filename
         try:
             name, extension = filename.split(".")
         except:
             extension = None
-        if extension = "jpg":
-            subprocess.call(["gst-launch", "v4l2src", "num-buffers=1", "!",
+        if extension == "jpg":
+            subprocess.call(["gst-launch-1.0", "v4l2src", "num-buffers=1", "!",
                              "jpegenc", "!", "filesink",
                              "location=" + self.filename])
-        elif extension = "png":
-            subprocess.call(["gst-launch", "v4l2src", "num-buffers=1",
+        elif extension == "png":
+            subprocess.call(["gst-launch-1.0", "v4l2src", "num-buffers=1",
                              "!", "pngenc", "!", "filesink",
                              "location=" + self.filename])
-        elif extension = None:
-            subprocess.call(["gst-launch", "v4l2src", "num-buffers=1", "!",
+        elif extension == None:
+            subprocess.call(["gst-launch-1.0", "v4l2src", "num-buffers=1", "!",
                              "jpegenc", "!", "filesink",
                              "location=" + self.filename + ".jpg"])
         else:
@@ -29,19 +28,18 @@ class LinuxCamera(Camera):
         self.on_complete()
 
     def _take_video(self, on_complete, filename=None):
-        assert(on_complete is not None)
         self.on_complete = on_complete
         self.filename = filename
         try:
             name, extension = filename.split(".")
         except:
             extension = None
-        if extension = None:
-            subprocess.call(["gst-launch", "v4l2src", "!", "ffmpegcolorspace",
+        if extension == None:
+            subprocess.call(["gst-launch-1.0", "v4l2src", "!", "videoconvert",
                              "!", "jpegenc", "!", "avimux", "!", "filesink",
                              "location=" + self.filename + ".avi"])
         else:
-            subprocess.call(["gst-launch", "v4l2src", "!", "ffmpegcolorspace",
+            subprocess.call(["gst-launch-1.0", "v4l2src", "!", "videoconvert",
                              "!", "jpegenc", "!", "avimux", "!", "filesink",
                              "location=" + self.filename])
         self.on_complete()
