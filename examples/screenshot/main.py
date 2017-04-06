845f1e441322d26662a82ddbe6b6de34c473d829
bhaveshAn <bhaveshanand7@gmail.com>
2017-04-07 00:11:22 +0530

    making example working without kv file

--- examples/screenshot/main.py
+++ examples/screenshot/main.py
@@ -1,77 +1,44 @@
-'''
-Basic screenshot example
-Default picture is saved as
-/sdcard/org.test.cameraexample/enter_file_name_here.jpg
-'''
-
-from os import getcwd
-from os.path import exists
-from os.path import splitext
-
-import kivy
-kivy.require('1.8.0')
-
 from kivy.app import App
+from kivy.lang import Builder
+from kivy.properties import NumericProperty
 from kivy.properties import ObjectProperty
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.popup import Popup
-from kivy.logger import Logger
-
-from plyer import screenshot
+from kivy.uix.boxlayout import BoxLayout
 
 
-class ScreenShotDemo(FloatLayout):
-    def __init__(self):
-        super(ScreenShotDemo, self).__init__()
-        self.cwd = getcwd() + "/"
-        self.ids.path_label.text = self.cwd
+Builder.load_string('''
+#:import screenshot plyer.screenshot
+<ScreenShotDemo>:
+    screenshot: screenshot
+    orientation: 'vertical'
+    padding: '50dp'
+    spacing: '20dp'
+    Label:
+        id: location_label
+        size_hint_y: None
+        height: sp(40)
+        text: 'ScreenShot Location: ' + str(root.screenshot.file_path)
 
-    def do_capture(self):
-        filepath = self.cwd + self.ids.filename_text.text
-        ext = splitext(filepath)[-1].lower()
-
-        if(exists(filepath)):
-            popup = MsgPopup("Picture with this name already exists!")
-            popup.open()
-            return False
-
-        try:
-            screenshot.take_shot(filename=filepath,
-                                on_complete=self.camera_callback)
-        except NotImplementedError:
-            popup = MsgPopup(
-                "This feature has not yet been implemented for this platform.")
-            popup.open()
-
-    def camera_callback(self, filepath):
-        if(exists(filepath)):
-            popup = MsgPopup("Picture saved!")
-            popup.open()
-        else:
-            popup = MsgPopup("Could not save your picture!")
-            popup.open()
+    Button:
+        id: record_button
+        text: 'Take Shot'
+        on_release: root.take_shot()
+''')
 
 
-class ScreenshotDemoApp(App):
-    def __init__(self):
-        super(ScreenshotDemoApp, self).__init__()
-        self.demo = None
+class ScreenShotDemo(BoxLayout):
+    '''Root Widget.'''
+
+    screenshot = ObjectProperty()
+    def take_shot(self):
+        self.screenshot.take_shot()
+    
+class ScreenShotApp(App):
 
     def build(self):
-        self.demo = ScreenShotDemo()
-        return self.demo
+        return ScreenShotDemo()
 
     def on_pause(self):
         return True
 
-    def on_resume(self):
-        pass
-
-
-class MsgPopup(Popup):
-    def __init__(self, msg):
-        super(MsgPopup, self).__init__()
-        self.ids.message_label.text = msg
-
-if __name__ == '__main__':
-    ScreenshotDemoApp().run()
+if __name__ == "__main__":
+    ScreenShotApp().run()
