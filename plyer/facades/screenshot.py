41fca16467d1280daa1faccc282fa1d8a881d73b
bhaveshAn <bhaveshanand7@gmail.com>
2017-04-08 08:54:18 +0530

    screenshot using xwd utility found on most linux distros

---
+++ plyer/facades/screenshot.py
@@ -0,0 +1,60 @@
+'''
+Screenshot
+=====
+
+The :class:`Screenshot` is used for recording audio.
+
+Default path for taking screenshot is set in platform implementation.
+
+Simple Examples
+---------------
+
+To get the file path::
+
+    >>> screenshot.file_path
+    '/sdcard/test.jpg'
+
+To set the file path::
+
+    >>> import os
+    >>> current_list = os.listdir('.')
+    ['/sdcard/testrecorder.jpg', '/sdcard/testrecorder1.jpg',
+    '/sdcard/testrecorder2.jpg', '/sdcard/testrecorder3.jpg']
+    >>> file_path = current_list[2]
+    >>> screenshot.file_path = file_path
+
+To take screenshot::
+
+    >>> from plyer import screenshot
+    >>> screenshot.take_shot()
+'''
+
+
+class Screenshot(object):
+    '''
+    ScreenShot facade.
+    '''
+    _file_path = ''
+
+    def __init__(self, file_path):
+        super(Screenshot, self).__init__()
+        self._file_path = file_path
+
+    def take_shot(self):
+        self._take_shot()
+
+    @property
+    def file_path(self):
+        return self._file_path
+
+    @file_path.setter
+    def file_path(self, location):
+        '''
+        Location of the screenshot.
+        '''
+        self._file_path = str(location)
+
+    # private
+
+    def _take_shot(self, **kwargs):
+        raise NotImplementedError()
---
+++ plyer/platforms/linux/screenshot.py
@@ -0,0 +1,18 @@
+import subprocess
+from plyer.facades import Screenshot
+from plyer.utils import whereis_exe
+
+
+class XwdScreenshot(Screenshot):
+    def __init__(self, file_path=None):
+        default_path = 'test.jpg'
+        super(XwdScreenshot, self).__init__(file_path or default_path)
+
+    def _take_shot(self):
+    	subprocess.call(["xwd", "-root", "-out", "screenshot.xwd"])
+    	subprocess.call(["convert", "screenshot.xwd", self.file_path])
+    	subprocess.call(["rm", "screenshot.xwd"])
+
+
+def instance():
+	return XwdScreenshot()
