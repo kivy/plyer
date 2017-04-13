c2494c6518360a54b81313295be47f9dfafe1909
malverick <sumitlnmiit96@gmail.com>
2017-04-14 00:20:16 +0530

    Add networkinfo facade

--- plyer/__init__.py
+++ plyer/__init__.py
@@ -8,7 +8,7 @@ __all__ = ('accelerometer', 'audio', 'barometer', 'battery', 'call', 'camera',
            'compass', 'email', 'filechooser', 'flash', 'gps', 'gravity',
            'gyroscope', 'irblaster', 'light', 'orientation', 'notification',
            'proximity', 'sms', 'tts', 'uniqueid', 'vibrator', 'wifi',
-           'temperature')
+           'temperature', 'networkinfo')
 
 __version__ = '1.3.1dev'
 
@@ -87,3 +87,6 @@ wifi = Proxy('wifi', facades.Wifi)
 
 #: Temperature proxy to :class:`plyer.facades.Temperature`
 temperature = Proxy('temperature', facades.Temperature)
+
+#: NetworkInfo proxy to :class:`plyer.facades.NetworkInfo`
+networkinfo = Proxy('networkinfo', facades.NetworkInfo)
