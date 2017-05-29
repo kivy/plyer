0f0c4de625a1b0edcd81a86adffcb8b5d7690b75
malverick <sumitlnmiit96@gmail.com>
2017-05-29 19:57:48 +0530

    Add documentation

--- plyer/facades/brightness.py
+++ plyer/facades/brightness.py
@@ -1,12 +1,40 @@
+'''
+Brightness
+==========
+
+This API helps you to control the brightness of your primary display screen.
+
+The :class:`Brightness` provides access to public methods to control the
+brightness of screen.
+
+Simple Examples
+---------------
+
+To set the brightness level to half of maximum::
+
+    >>> from plyer import brightness
+    >>> brightness.set(50)
+
+'''
+
+
 class Brightness(object):
-	'''
-	Brightness facade.
-	'''
+    '''
+    Brightness facade.
+    '''
 
-	def set(self, level):
-		return self._set(level)
+    def set(self, level):
+        '''
+        Adjust the brightness of the screen.
+        Minimum brightnesss level:: 1
+        Maximum brightness level:: 100
 
-	#private
+        :param level: New level of brightness between 1 and 100
+        :type level: int
+        '''
+        return self._set(level)
 
-	def _set(self, level):
-		raise NotImplementedError()
+    #private
+
+    def _set(self, level):
+        raise NotImplementedError()
