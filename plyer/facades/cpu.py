123018c543455bcccff139ba57a061d04712b41b
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2018-08-12 04:47:58 +0200

    Add CPU count for Windows

--- plyer/facades/cpu.py
+++ plyer/facades/cpu.py
@@ -20,6 +20,7 @@ Supported Platforms
 
 MacOS
 Linux
+Windows
 '''
 
 
@@ -56,6 +57,23 @@ class CPU(object):
         '''
         return self._logical()
 
+    @property
+    def cache(self):
+        '''
+        Property that contains the count of L1, L2, L3 caches in the system
+        as a dictionary `{'L1': int, 'L2': int, 'L3': int}`.
+        '''
+        return self._cache()
+
+    @property
+    def numa(self):
+        '''
+        Property that contains the count of NUMA nodes in the system.
+
+        .. note:: https://en.wikipedia.org/wiki/Non-uniform_memory_access
+        '''
+        return self._numa()
+
     # private
 
     def _sockets(self):
@@ -67,3 +85,8 @@ class CPU(object):
     def _logical(self):
         raise NotImplementedError()
 
+    def _cache(self):
+        raise NotImplementedError()
+
+    def _numa(self):
+        raise NotImplementedError()
