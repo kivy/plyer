adef27b21d52f20d99323564626892c07b5d5b19
Maroš Kováč <maros1305@gmail.com>
2018-10-05 12:22:23 +0200

    Audio recording working, minor improvements needed

--- plyer/platforms/macosx/audio.py
+++ plyer/platforms/macosx/audio.py
@@ -1,6 +1,6 @@
 from plyer.facades import Audio
 from pyobjus.dylib_manager import load_framework, INCLUDE
-from pyobjus import autoclass
+from pyobjus import autoclass, objc_str
 
 load_framework(INCLUDE.Foundation)
 load_framework(INCLUDE.AVFoundation)
@@ -12,21 +12,35 @@ NSURL = autoclass('NSURL')
 
 class OSXAudio(Audio):
     def __init__(self, file_path=None):
-        default_path = '~/Desktop/testrecording.wav'
+        default_path = '/Users/maroskovac/Dev/audio.wav'
         super(OSXAudio, self).__init__(file_path or default_path)
 
         self._recorder = None
         self._player = None
 
     def _start(self):
-        filePathNSString = NSString.alloc().initWithUTF8String_(self._file_path)
-        fileNSURL = NSURL.alloc().initFileURLWithPath_(filePathNSString)
-        af = AVAudioFormat.alloc()
-        af = af.initWithCommonFormat_sampleRate_channels_interleaved_(
-            1, 44100, 1, True
+        # filePathNSString = NSString.alloc().initWithUTF8String_(self._file_path)
+
+        fileNSURL = NSURL.alloc().initWithString_(
+            objc_str(self._file_path)
         )
 
-        self._recorder = AVAudioRecorder.alloc().initWithURL_format_error_(fileNSURL, fileAudioFormat, None)
-    
+        af = AVAudioFormat.alloc()
+        af = af.initWithCommonFormat_sampleRate_channels_interleaved_(
+            1, 44100.0, 2, True
+        )
+
+        self._recorder = AVAudioRecorder.alloc().initWithURL_format_error_(fileNSURL, af, None)
+        
+        if self._recorder:
+            print("Recording...")
+            self._recorder.record()
+
+    def _stop(self):
+        if self._recorder:
+            self._recorder.stop()
+            print("Stopped recording.")
+            self._recorder = None
+
 def instance():
-    return OSXAudio();
+    return OSXAudio()
