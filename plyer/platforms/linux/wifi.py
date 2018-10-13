a73a97c0d3c1d6d1e234443be3ad9cbb4b9c773e
Vibhu Agarwal <vibhu4agarwal@gmail.com>
2018-10-13 14:57:56 +0530

    Update wifi.py to make it python3 compatible

--- plyer/platforms/linux/wifi.py
+++ plyer/platforms/linux/wifi.py
@@ -23,7 +23,7 @@ class LinuxWifi(Wifi):
         Returns `True` if wifi is enabled else `False`.
         '''
         enbl = Popen(["nmcli", "radio", "wifi"], stdout=PIPE, stderr=PIPE)
-        if enbl.communicate()[0].split()[0] == "enabled":
+        if enbl.communicate()[0].split()[0].decode() == "enabled":
             return True
         return False
 
