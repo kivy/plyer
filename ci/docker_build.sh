c1524934f313a59fa070c37b4a03e619fd512f66
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2018-11-07 01:06:39 +0100

    Move Dockerfiles to separate CI folder

--- ci/docker_build.sh
+++ ci/docker_build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # build docker images from Travis matrix
-if [ "${TRAVIS_OS_NAME}" == "linux" ] && [ -v DOCK ]
+if [ "$TRAVIS_OS_NAME" == "linux" ] && [ -v DOCK ]
 then
 
     # separate images for Python 2
@@ -9,24 +9,24 @@ then
     then
         docker build \
             --tag plyer:py2 \
-            --file Dockerfile.$IMAGE.py2 \
-            "$(pwd)"
+            --file docker/Dockerfile.$IMAGE.py2 \
+            "$(pwd)/.."
 
     # separate images for Python 3
     elif [ "$PY" == "3" ]
     then
         docker build \
             --tag plyer:py3 \
-            --file Dockerfile.$IMAGE.py3 \
-            "$(pwd)"
+            --file docker/Dockerfile.$IMAGE.py3 \
+            "$(pwd)/.."
 
         # style image that inherits layers from Python 3 image
         if [ "$RUN" == "style" ]
         then
             docker build \
                 --tag plyer:style \
-                --file Dockerfile.$IMAGE.style \
-                "$(pwd)"
+                --file docker/Dockerfile.$IMAGE.style \
+                "$(pwd)/.."
         fi
     fi
 fi
