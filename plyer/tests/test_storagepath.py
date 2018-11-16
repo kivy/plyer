fd6fbaf2dcf83f05d117d28d80ad0b361d558a76
Maroš Kováč <maros1305@gmail.com>
2018-11-16 17:49:13 +0100

    Added macOS storagepath test

---
+++ plyer/tests/test_storagepath.py
@@ -0,0 +1,44 @@
+'''
+TestStoragePath
+===============
+
+Tested platforms:
+
+* macOS
+'''
+
+import unittest
+
+from plyer.tests.common import platform_import
+
+class TestStoragePath(unittest.TestCase):
+    '''
+    TestCase for plyer.storagepath.
+    '''
+    def test_storagepath_macosx(self):
+        '''
+        Test macOS for plyer.storagepath.
+        '''
+        storagepath = platform_import(
+            platform='macosx',
+            module_name='storagepath'
+        )
+
+        self.assertIn('OSXStoragePath', dir(storagepath))
+        storagepath = storagepath.instance()
+        self.assertIn('OSXStoragePath', str(storagepath))
+
+        path_format = 'file:///Users/'
+
+        self.assertIn(path_format, storagepath.get_home_dir())
+        self.assertIn('/', storagepath.get_root_dir())
+        self.assertIn(path_format, storagepath.get_documents_dir())
+        self.assertIn(path_format, storagepath.get_downloads_dir())
+        self.assertIn(path_format, storagepath.get_videos_dir())
+        self.assertIn(path_format, storagepath.get_music_dir())
+        self.assertIn(path_format, storagepath.get_pictures_dir())
+        self.assertIn(path_format, storagepath.get_application_dir())
+
+
+if __name__ == '__main__':
+    unittest.main()
