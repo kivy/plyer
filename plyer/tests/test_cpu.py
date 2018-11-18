83c7d656e91010c89ac2a8ce911a4f7578547dcc
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2018-11-18 02:01:57 +0100

    Add test for GNU/Linux CPU physical cores (kernel cpuinfo)

--- plyer/tests/test_cpu.py
+++ plyer/tests/test_cpu.py
@@ -8,8 +8,11 @@ Tested platforms:
 * Linux - nproc
 '''
 
+import sys
 import unittest
 from os import environ
+from mock import patch, Mock
+from textwrap import dedent
 
 from plyer.tests.common import PlatformTest, platform_import
 
@@ -54,12 +57,97 @@ class MockedNProc(object):
         return int(MockedNProc.logical_cores)
 
 
+class MockedProcinfo(object):
+    # docs:
+    # https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
+    # /tree/arch/x86/kernel/cpu/proc.c
+    sockets = 1  # physical id
+    physical = 2  # core id
+    threads_per_core = 2  # Intel specs document for i7-4500U
+    logical = physical * threads_per_core  # processor
+
+    def __init__(self, *args, **kwargs):
+        self.fname = args[0] if args else ''
+
+        self.output = []
+        __step = 0  # 0,1,0,1 -> 0,0,1,1
+        for soc in range(self.sockets):
+            for log in range(self.logical):
+                if log != 0 and not log % self.physical:
+                    __step += 1
+                self.output.append((dedent(
+                    '''\
+                    processor\t: {logical}
+                    vendor_id\t: GenuineIntel
+                    cpu family\t: 6
+                    model\t\t: 69
+                    model name\t: Intel(R) Core(TM) i7-4500U CPU @ 1.80GHz
+                    stepping\t: 1
+                    microcode\t: 0x17
+                    cpu MHz\t\t: 774.000
+                    cache size\t: 4096 KB
+                    physical id\t: {socket}
+                    siblings\t: 4
+                    core id\t\t: {physical}
+                    cpu cores\t: {threads_per_core}
+                    apicid\t\t: {logical}
+                    initial apicid\t: 0
+                    fpu\t\t: yes
+                    fpu_exception\t: yes
+                    cpuid level\t: 13
+                    wp\t\t: yes
+                    flags\t\t: fpu vme de pse tsc msr pae mce cx8 ...
+                    bogomips\t: 3591.40
+                    clflush size\t: 64
+                    cache_alignment\t: 64
+                    address sizes\t: 39 bits physical, 48 bits virtual
+                    power management:
+                    \n'''
+                )).format(**{
+                    'socket': soc,
+                    'physical': __step,
+                    'logical': log,
+                    'threads_per_core': self.threads_per_core
+                }))
+        self.output = ''.join(self.output).encode('utf-8')
+
+    def __enter__(self, *args):
+        file_value = None
+
+        if self.fname == '/proc/cpuinfo':
+            file_value = Mock()
+            file_value.readlines.return_value = self.output.split(
+                '\n'.encode('utf-8')
+            )
+        return file_value
+
+    def __exit__(self, *args):
+        pass
+
+
 class TestCPU(unittest.TestCase):
     '''
     TestCase for plyer.cpu.
     '''
 
-    @PlatformTest('linux')
+    def test_cpu_linux_physical(self):
+        cpu = platform_import(
+            platform='linux',
+            module_name='cpu',
+            whereis_exe=lambda b: b == 'nproc'
+        ).instance()
+
+        stub = MockedProcinfo
+        py2_target = '__builtin__.open'
+        py3_target = 'builtins.open'
+        target = py3_target if sys.version_info.major == 3 else py2_target
+
+        with patch(target=target, new=stub):
+            sb = stub()
+            self.assertEqual(
+                cpu.physical, sb.physical
+            )
+
     def test_cpu_linux_logical(self):
         '''
         Test mocked Linux NProc for plyer.cpu.
