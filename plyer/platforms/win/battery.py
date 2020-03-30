97629a5d83b4f94d9655f8ef73c53666802a69f3
irm19 <ismaelruizmartin@hotmail.com>
2020-03-31 06:06:14 +1300

    Modification of Status isCharging in windows (#546)
    * Modification of Status isCharging in windows
    
    Modification to adapt that the source of information is a flag that includes multiple conditions. See.
    https://docs.microsoft.com/en-us/windows/win32/api/winbase/ns-winbase-system_power_status
    
    * Update battery.py
    
    * Update battery.py
    
    * Update battery.py
    
    * Update battery.py
    
    * Update battery.py
    
    * Update battery.py
    
    * Update battery.py
    
    * Update battery.py
    
    * Update battery.py
    
    * Update battery.py
    
    * Update battery.py

--- plyer/platforms/win/battery.py
+++ plyer/platforms/win/battery.py
@@ -4,6 +4,7 @@ Module of Windows API for plyer.battery.
 
 from plyer.platforms.win.libs.batterystatus import battery_status
 from plyer.facades import Battery
+from ctypes.wintypes import BYTE
 
 
 class WinBattery(Battery):
@@ -12,6 +13,8 @@ class WinBattery(Battery):
     '''
 
     def _get_state(self):
+        CHARGING = BYTE(8).value
+        UNKNOWN_STATUS = BYTE(255).value
         status = {"isCharging": None, "percentage": None}
 
         query = battery_status()
@@ -19,7 +22,8 @@ class WinBattery(Battery):
         if not query:
             return status
 
-        status["isCharging"] = query["BatteryFlag"] == 8
+        status["isCharging"] = (query["BatteryFlag"] != UNKNOWN_STATUS) and \
+                               (query["BatteryFlag"] & CHARGING > 0)
         status["percentage"] = query["BatteryLifePercent"]
 
         return status
