538a1874a1527d40a0a7ac5931158a3fbdb82c03
Guhan Sensam <guhansensam@gmail.com>
2021-04-14 13:17:37 +0530

    Removed Whitespaces

--- plyer/platforms/android/filechooser.py
+++ plyer/platforms/android/filechooser.py
@@ -159,8 +159,6 @@ class AndroidFileChooser(FileChooser):
                 selection.append(ele)
         except:
             selection = [self._resolve_uri(data.getData()),]
-        
-        
 
         # return value to object
         self.selection = selection
