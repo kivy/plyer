da57a4c7e1c49ec148dcfbeca9c03d6c7c66585b
Lakhya Jyoti Nath <lakhya.sci@gmail.com>
2021-06-17 23:48:26 +0530

    Added linux api for DeviceName

---
+++ plyer/platforms/linux/devicename.py
@@ -0,0 +1,25 @@
+'''
+Module of Linux API for plyer.devicename.
+'''
+
+import socket
+from plyer.facades import DeviceName
+
+class LinuxDeviceName(DeviceName):
+    '''
+    Implementation of Linux DeviceName API.
+    '''
+
+    def _get_device_name(self):
+        hostname = None
+        
+        if not socket.gethostname():
+            hostname = socket.gethostname()
+            
+        return hostname
+
+def instance():
+    '''
+    Instance for facade proxy.
+    '''
+    return LinuxDeviceName()
