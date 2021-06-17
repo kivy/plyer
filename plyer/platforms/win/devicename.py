5fe1d83234ed3712c37ef48c5f53906716b2cdea
Lakhya Jyoti Nath <lakhya.sci@gmail.com>
2021-06-17 23:48:38 +0530

    Added Wndows api for DeviceName

---
+++ plyer/platforms/win/devicename.py
@@ -0,0 +1,25 @@
+'''
+Module of Win API for plyer.devicename.
+'''
+
+import socket
+from plyer.facades import DeviceName
+
+class WinDeviceName(DeviceName):
+    '''
+    Implementation of Linux DeviceName API.
+    '''
+
+    def _get_device_name(self):
+        hostname = None
+        
+        if not socket.gethostname():
+            hostname = socket.gethostname()
+            
+        return hostname
+
+def instance():
+    '''
+    Instance for facade proxy.
+    '''
+    return WinDeviceName()
