51b5d6f4f430056929d6ec876b26065989c7fb66
Lakhya Jyoti Nath <lakhya.sci@gmail.com>
2021-06-18 18:47:47 +0530

    added unit test for devicename

---
+++ plyer/tests/test_devicename.py
@@ -0,0 +1,43 @@
+'''
+TestDeviceName
+============
+
+Tested platforms:
+
+* Windows
+'''
+
+import unittest
+from mock import patch, Mock
+from plyer.tests.common import PlatformTest, platform_import
+import socket
+
+class TestDeviceName(unittest.TestCase):
+    '''
+    TestCase for plyer.devicename.
+    '''
+
+    def test_devicename(self):
+        '''
+        General all platform test for plyer.devicename.
+        '''
+        from plyer import devicename
+        self.assertTrue(len(devicename.device_name) > 0)
+
+    @PlatformTest('win')
+    def test_devicename_win(self):
+        '''
+        Test Windows API for plyer.devicename.
+        '''
+        devicename = platform_import(
+            platform='Win',
+            module_name='devicename',
+        )
+        
+        with patch.object(socket, 'gethostname', return_value='mocked_hostname') as mock_method:
+            evaluated_device_name = devicename.device_name
+            self.assertEqual(evaluated_device_name, 'mocked_hostname')
+        
+
+if __name__ == '__main__':
+    unittest.main()
