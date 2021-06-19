712e7c9fc8b8bcbd5894ceab297656a8ea6ed6fa
Lakhya Jyoti Nath <lakhya.sci@gmail.com>
2021-06-19 14:19:46 +0530

    Corrected unit tests

--- plyer/tests/test_devicename.py
+++ plyer/tests/test_devicename.py
@@ -8,7 +8,7 @@ Tested platforms:
 '''
 
 import unittest
-from mock import patch, Mock
+from mock import patch
 from plyer.tests.common import PlatformTest, platform_import
 import socket
 
@@ -18,28 +18,41 @@ class TestDeviceName(unittest.TestCase):
     TestCase for plyer.devicename.
     '''
 
-    def test_devicename(self):
-        '''
-        General all platform test for plyer.devicename.
-        '''
-        from plyer import devicename
-        self.assertTrue(len(devicename.device_name) > 0)
-
     @PlatformTest('win')
     def test_devicename_win(self):
         '''
         Test Windows API for plyer.devicename.
         '''
-        devicename = platform_import(
-            platform='Win',
-            module_name='devicename',
-        )
+        devicename = platform_import(platform='win',
+                                     module_name='devicename'
+                                     )
+        devicename_instance = devicename.instance()
+
         with patch.object(socket,
                           'gethostname',
-                          return_value='mocked_hostname'
+                          return_value='mocked_windows_hostname'
                           ) as mock_method:
-            evaluated_device_name = devicename.device_name
-            self.assertEqual(evaluated_device_name, 'mocked_hostname')
+
+            evaluated_device_name = devicename_instance._get_device_name()
+            self.assertEqual(evaluated_device_name, 'mocked_windows_hostname')
+
+    @PlatformTest('linux')
+    def test_devicename_linux(self):
+        '''
+        Test Linux API for plyer.devicename.
+        '''
+        devicename = platform_import(platform='linux',
+                                     module_name='devicename'
+                                     )
+        devicename_instance = devicename.instance()
+
+        with patch.object(socket,
+                          'gethostname',
+                          return_value='mocked_linux_hostname'
+                          ) as mock_method:
+
+            evaluated_device_name = devicename_instance._get_device_name()
+            self.assertEqual(evaluated_device_name, 'mocked_linux_hostname')
 
 
 if __name__ == '__main__':
