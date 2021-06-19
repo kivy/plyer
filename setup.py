c11a27df04827e31d70adb128a44a706998897b5
Lakhya Jyoti Nath <lakhya.sci@gmail.com>
2021-06-19 20:15:48 +0530

    Removed python2.6|7 reference and added reference for python 3.6|7|8

--- setup.py
+++ setup.py
@@ -74,12 +74,13 @@ setup(
         'Natural Language :: English',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     **EXTRA_OPTIONS
 )
