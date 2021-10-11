35daf3eee023dd4e3ecb47e87b89d16d4b1e5f27
Antonio Cuni <anto.cuni@gmail.com>
2021-10-11 22:48:20 +0200

    Make AndroidCamera compatible with android API >= 24 by allowing to specify a FILEPROVIDER_AUTHORITY and to get the uri accordingly

--- plyer/platforms/android/camera.py
+++ plyer/platforms/android/camera.py
@@ -1,7 +1,7 @@
 import android
 import android.activity
 from os import remove
-from jnius import autoclass, cast
+from jnius import autoclass, cast, JavaException
 from plyer.facades import Camera
 from plyer.platforms.android import activity
 
@@ -9,9 +9,13 @@ Intent = autoclass('android.content.Intent')
 PythonActivity = autoclass('org.kivy.android.PythonActivity')
 MediaStore = autoclass('android.provider.MediaStore')
 Uri = autoclass('android.net.Uri')
+File = autoclass('java.io.File')
+FileProvider = autoclass('android.support.v4.content.FileProvider')
 
 
 class AndroidCamera(Camera):
+
+    FILEPROVIDER_AUTHORITY = None
 
     def _take_picture(self, on_complete, filename=None):
         assert(on_complete is not None)
@@ -20,7 +24,7 @@ class AndroidCamera(Camera):
         android.activity.unbind(on_activity_result=self._on_activity_result)
         android.activity.bind(on_activity_result=self._on_activity_result)
         intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        uri = Uri.parse('file://' + filename)
+        uri = self._get_uri_for_file(filename)
         parcelable = cast('android.os.Parcelable', uri)
         intent.putExtra(MediaStore.EXTRA_OUTPUT, parcelable)
         activity.startActivityForResult(intent, 0x123)
@@ -32,7 +36,7 @@ class AndroidCamera(Camera):
         android.activity.unbind(on_activity_result=self._on_activity_result)
         android.activity.bind(on_activity_result=self._on_activity_result)
         intent = Intent(MediaStore.ACTION_VIDEO_CAPTURE)
-        uri = Uri.parse('file://' + filename)
+        uri = self._get_uri_for_file(filename)
         parcelable = cast('android.os.Parcelable', uri)
         intent.putExtra(MediaStore.EXTRA_OUTPUT, parcelable)
 
@@ -53,6 +57,28 @@ class AndroidCamera(Camera):
             remove(fn)
         except OSError:
             pass
+
+    def _get_uri_for_file(self, filename):
+        if self.FILEPROVIDER_AUTHORITY is None:
+            # default behavior, backward-compatible: works with Android <= 9
+            return Uri.parse('file://' + filename)
+        #
+        # For Android >= 10, we need to declare a FileProvider in
+        # AndroidManifest and set plyer.camera.FILEPROVIDER_AUTHORITY accordingly
+        currentActivity = cast('android.app.Activity', PythonActivity.mActivity)
+        ctx = currentActivity.getApplicationContext()
+        try:
+            return FileProvider.getUriForFile(ctx, self.FILEPROVIDER_AUTHORITY,
+                                              File(filename))
+        except JavaException as exc:
+            raise Exception(
+                f'Cannot get a uri for filename {filename} for the Fileprovider '
+                f'authority {self.FILEPROVIDER_AUTHORITY}.  This probably means that '
+                f'FILE_PROVIDER_PATHS is not configured correctly in '
+                f'AndroidManifest.xml and/or you need to change the value of '
+                f'plyer.camera.FILEPROVIDER_AUTHORITY'
+            )
+        return uri
 
 
 def instance():
