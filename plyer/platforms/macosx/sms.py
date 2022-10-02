eb069e362877cacfb53f4f195ab8d483f43d13cf
Rohan Shah <57906961+rshah713@users.noreply.github.com>
2022-10-02 15:08:24 -0400

    2nd pass at style fixes

--- plyer/platforms/macosx/sms.py
+++ plyer/platforms/macosx/sms.py
@@ -7,21 +7,22 @@ class OSXSMS(SMS):
     '''
     Implementation of macOS' Messages API
     '''
-    
+
     def _send(self, **kwargs):
         '''
         Will send `message` to `recipient` via Messages app
-        
+
         Currently only supports iMessage as macOS can send that standalone
-        In order to support SMS, a valid carrier-activated device must be connected & configured to macOS
+        In order to support SMS, a valid carrier-activated device must be
+        connected & configured to macOS
             - activate this by passing in mode='SMS'
-        
+
         '''
-        
+
         recipient = kwargs.get('recipient')
         message = kwargs.get('message')
         mode = kwargs.get('mode', 'iMessage') # can be SMS
-       
+
         APPLESCRIPT = f"""tell application "Messages"
     set targetService to 1st account whose service type = {mode}
     set targetBuddy to participant "{recipient}" of targetService
@@ -29,10 +30,10 @@ class OSXSMS(SMS):
 end tell"""
 
         osascript_process = Popen(['osascript', '-e', APPLESCRIPT],
-        stdout=PIPE, stderr=PIPE)
+                                    stdout=PIPE, stderr=PIPE)
         stdout, stderr = osascript_process.communicate()
-        
-        
+
+
 def instance():
     import sys
     if whereis_exe('osascript'):
