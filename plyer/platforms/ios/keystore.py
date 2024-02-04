824ca7c02eaf3ec2bf1b91a9527e31431438befb
t0theRANCH <tyson.carter@live.ca>
2024-02-04 16:35:34 -0500

    uses native ios and android keyring/keystore functionality to store sensitive data

--- plyer/platforms/ios/keystore.py
+++ plyer/platforms/ios/keystore.py
@@ -9,26 +9,7 @@ class IosKeystore(Keystore):
     """
     In order to get this to work, I had to write an Objective-C class that contains the functions to interact with the
     keychain. They were a part of a framework, and not a class, and are thus, unable to be accessed with pyobjus
-    normally. If for some reason you want to do this yourself, the original file (KeychainBridge.m) is in the 'tools'
-    directory. You can compile it yourself using the following commands(macOS):
-
-    First compile the ios-sim dylib:
-    clang -target x86_64-apple-ios13.0-simulator -isysroot $(xcrun --sdk iphonesimulator --show-sdk-path) \
-    -F$(xcrun --sdk iphonesimulator --show-sdk-path)/System/Library/Frameworks -framework Security -framework Foundation \
-    -shared -o KeychainBridge_sim.dylib KeychainBridge.m
-
-
-    Then compile the ios dylib:
-    clang -target arm64-apple-ios13.0 -isysroot $(xcrun --sdk iphoneos --show-sdk-path) -F$(xcrun --sdk iphoneos \
-    --show-sdk-path)/System/Library/Frameworks -framework Security -framework Foundation -shared -o \
-    KeychainBridge_device.dylib KeychainBridge.m
-
-    then run this command to create a fat dylib:
-    lipo -create -output KeychainBridge.dylib KeychainBridge_sim.dylib KeychainBridge_device.dylib
-
-    This will give you a bridge from Objective-C to Python that will allow you to interact with the keychain,
-    this will work on both the Xcode iOS simulator (x86_64) and an iPhone (arm64). If you only want one or the other,
-    you can just use the sim or device dylib and ignore the other two commands.
+    normally. I compiled it into a .framework to comply with Apple's App Store guidelines. This will give you a bridge from Objective-C to Python that will allow you to interact with the keychain.
 
     It comes with three functions, you can see usage examples in the class below:
     - (BOOL)saveWithService:(NSString *)service account:(NSString *)account value:(NSString *)value;
@@ -38,7 +19,7 @@ class IosKeystore(Keystore):
 
     @staticmethod
     def KeychainBridge():
-        with path('plyer.tools.KeychainBridge', 'KeychainBridge.framework') as framework_path:
+        with path('plyer.tools', 'KeychainBridge.framework') as framework_path:
             load_framework(str(framework_path))
             return autoclass('KeychainBridge')
 
