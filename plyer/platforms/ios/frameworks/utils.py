43f4b588d0a3f789b7592906b26e30a13a13d9c7
Sanquez Heard <135459789+SanquezH@users.noreply.github.com>
2025-02-25 14:13:24 -0600

    Flake8 Compliance

--- plyer/platforms/ios/frameworks/utils.py
+++ plyer/platforms/ios/frameworks/utils.py
@@ -3,6 +3,7 @@ from os.path import join, dirname
 from os import environ
 from pyobjus.dylib_manager import load_framework
 
+
 def load_plyer_framework(framework):
     framework_directory = join(dirname(ios.__file__), "frameworks")
     if 'SIMULATOR_DEVICE_NAME' in environ:
