212a7c73797407477b133a1a356f8c7944c06b73
Sanquez Heard <135459789+SanquezH@users.noreply.github.com>
2025-02-25 14:04:18 -0600

    Update main.py for Flake8 Compliance

--- examples/voip/main.py
+++ examples/voip/main.py
@@ -4,10 +4,11 @@ from kivy.uix.boxlayout import BoxLayout
 from plyer import voip
 import threading
 
+
 class VOIPClientApp(App):
     def build(self):
-        # Create a call and end call button to alternate between to allow client control over VOIP call
-        self.layout = BoxLayout(orientation='vertical')
+        # Create call and end-call buttons for VOIP client control.
+        self.layout = BoxLayout(orientation="vertical")
         self.call_button = Button(text="Call")
         self.end_call_button = Button(text="End Call", disabled=True)
         self.call_button.bind(on_press=self.start_call)
@@ -15,10 +16,9 @@ class VOIPClientApp(App):
         self.layout.add_widget(self.call_button)
         self.layout.add_widget(self.end_call_button)
         return self.layout
-    
-    def auto_end_call(self):  # Automate ending call, including if connection closes externally
-        # If client has microphone permissions and is connected to the server, the call is active
-        # Else, display an error dialog or message indicating which condition is not met
+
+    def auto_end_call(self):  # Auto end call if connection closes.
+        # If mic permission is granted and connected, the call is active.
         if voip.hasPermission and voip.connected:
             while voip.active_call:  # Loop that runs until call ends
                 pass
@@ -27,19 +27,21 @@ class VOIPClientApp(App):
             self.end_call(instance)
 
     def start_call(self, instance):
-	    # Disable call button after call button is pressed
+        # Disable call button after call button is pressed
         self.call_button.disabled = True
         self.end_call_button.disabled = False
         voip.start_call(  # Initiate the VOIP call
-            dst_address = "192.168.1.12",  # Set to your server's IP address. Use root domain if using SSL [Required]
-            dst_port = 8080,  # Set to your server's assigned port [Required]
-            timeout = 3,  # Sets wait time to connect to server (5 seconds is default) [Optional]
-            ssl = False,  # Determines if SSL/TLS will be used (False by default) [Optional]
-            tls_version = "" , # Defaults to auto selection if empty. "TLSv1.3" and "TLSv1.2" are available options. [Optional]
-            client_id = "user@kivy.org",  # Supports identifying/authorizing connection [Optional]
-            debug = True  # Displays kivy log statements to assist in troubleshooting (False by default) [Optional]
+            dst_address="192.168.1.2",  # Use root domain for SSL [Required]
+            dst_port=8080,  # Set to your server's assigned port [Required]
+            timeout=3,  # Sets wait time to connect to server (5 secs default)
+            ssl=False,  # Determines if TLS will be used (False by default)
+            tls_version="",  # Auto-select if empty; use "TLSv1.3" or "TLSv1.2"
+            client_id="user@kivy.org",  # For identification or authentication
+            debug=True,  # Show logs for troubleshooting (False by default)
         )
-        self.track_call_thread = threading.Thread(target=self.auto_end_call, daemon=True)
+        self.track_call_thread = threading.Thread(
+            target=self.auto_end_call, daemon=True
+        )
         self.track_call_thread.start()
 
     def end_call(self, instance):
@@ -47,6 +49,7 @@ class VOIPClientApp(App):
         self.end_call_button.disabled = True
         self.call_button.disabled = False
         voip.end_call()  # End the VOIP call
-        
+
+
 if __name__ == "__main__":
     VOIPClientApp().run()
